fd722772a4062cc00bbe983558d4405a0e8a38d0
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-11-30 20:19:02 -0800

    LC-search-a-2d-matrix [Runtime: 0 ms; Memory: 12.3 MB]

--- array/search-a-2d-matrix.py
+++ array/search-a-2d-matrix.py
@@ -1,20 +1,25 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
-        rows = len(matrix)
-        columns = len(matrix[0])
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rows, columns = len(matrix) - 1, len(matrix[0])
 
         top = 0
-        bottom = rows - 1
+        bottom = rows
 
-        
         while(top <= bottom):
+
             row = (top + bottom) // 2
 
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
+            if(target < matrix[row][0]):
                 bottom = row - 1
+            
+            elif(target > matrix[row][-1]):
+                top = row + 1
+            
             else:
                 break
         
@@ -22,21 +27,18 @@ class Solution:
         right = columns - 1
 
         while(left <= right):
+            
             mid = (left + right) // 2
 
-            if(target == matrix[row][mid]):
-                return True
-            elif(target > matrix[row][mid]):
+            if(target > matrix[row][mid]):
                 left = mid + 1
-            else:
+            
+            elif(target < matrix[row][mid]):
                 right = mid - 1
+            
+            else:
+                return True
         
         return False
-
-
-
-
-
-
-
+        
         
