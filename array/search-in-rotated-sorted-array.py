2335eb782b66a9bf63c94291fd599b28df1c6279
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-01 00:35:04 -0800

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.py
+++ array/search-in-rotated-sorted-array.py
@@ -1,12 +1,15 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         l = 0
         r = len(nums) - 1
-
         while(l <= r):
+            mid = (l + r) // 2
 
-            mid = (r + l) // 2
             if(target == nums[mid]):
                 return mid
             
@@ -16,9 +19,10 @@ class Solution:
                 else:
                     r = mid - 1
             else:
-                if(target < nums[mid] or target > nums[r]):
-                    r = mid - 1
-                else:
-                    l = mid + 1
+                r = mid - 1
+
+
+        
 
         return -1
+        
