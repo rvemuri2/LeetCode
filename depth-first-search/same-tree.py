ed417b56a39fd3cb6abc242a6250f8a974aa057b
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-01 21:24:38 -0800

    LC-same-tree [Runtime: 0 ms; Memory: 12.3 MB]

--- depth-first-search/same-tree.py
+++ depth-first-search/same-tree.py
@@ -1,23 +1,32 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
 
-        if not p and not q:
-            return True
-        
-        if not p and q:
-            return False
-        
-        if not q and p:
-            return False
-        
-        if q.val != p.val:
-            return False
+        def dfs(p, q):
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            if not p and not q:
+                return True
             
+            if not p and q:
+                return False
+            
+            if not q and p:
+                return False
+            
+            if p.val != q.val:
+                return False
+            
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+        
+        return dfs(p, q)
+        
