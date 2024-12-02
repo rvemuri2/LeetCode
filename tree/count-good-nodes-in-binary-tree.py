8b50be09deddb638fb2100496cdef2e0dc532e3e
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-02 13:07:42 -0800

    LC-count-good-nodes-in-binary-tree [Runtime: 122 ms; Memory: 31.7 MB]

---
+++ tree/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(root, maxVal):
+
+            if not root:
+                return 0
+            
+            good = 1 if root.val >= maxVal else 0
+
+            maxVal = max(maxVal, root.val)
+            
+            good += dfs(root.left, maxVal)
+            good += dfs(root.right, maxVal)
+
+            return good
+
+        return dfs(root, root.val)
+
+
+
+        
