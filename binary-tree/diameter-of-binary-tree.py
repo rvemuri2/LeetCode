e56460b9d2b2c8173cab7940a25afae36e239c1a
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-01 21:02:12 -0800

    LC-diameter-of-binary-tree [Runtime: 10 ms; Memory: 25.9 MB]

--- binary-tree/diameter-of-binary-tree.py
+++ binary-tree/diameter-of-binary-tree.py
@@ -1,26 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
 
-        self.res = 0
+        self.diameter = 0
 
         def dfs(root):
-
             if not root:
                 return 0
             
             left = dfs(root.left)
             right = dfs(root.right)
 
-            self.res = max(self.res, left + right)
-
+            self.diameter = max(self.diameter, (left + right))
 
             return 1 + max(left, right)
-        
+
         dfs(root)
-        return self.res
+        return self.diameter
