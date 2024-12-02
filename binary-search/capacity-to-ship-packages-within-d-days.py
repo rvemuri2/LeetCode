f129f4261bb1d10d2e6b09b2353e90d8e5ed1a44
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-02 12:22:36 -0800

    LC-capacity-to-ship-packages-within-d-days [Runtime: 195 ms; Memory: 21.7 MB]

---
+++ binary-search/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+
+        right = sum(weights)
+        left = max(weights)
+        res = float('infinity')
+
+
+        def canShip(cap):
+            ships, currCap = 1, cap
+            for w in weights:
+                if(currCap - w < 0):
+                    ships += 1
+                    currCap = cap
+                currCap -= w
+            
+            if(ships <= days):
+                return True
+            else:
+                return False
+
+        while(left <= right):
+            mid = (right + left) // 2
+
+            if(canShip(mid)):
+                res = min(res, mid)
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return res
