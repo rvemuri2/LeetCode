c63ab160ec774e52281149f33939cadfc030b52a
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-01 20:31:32 -0800

    LC-time-based-key-value-store [Runtime: 240 ms; Memory: 72.7 MB]

--- string/time-based-key-value-store.py
+++ string/time-based-key-value-store.py
@@ -1,29 +1,31 @@
-class TimeMap:
+class TimeMap(object):
 
     def __init__(self):
         self.hash = {}
         
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
+    def set(self, key, value, timestamp):
         self.hash[key] = self.hash.get(key, [])
         self.hash[key].append([value, timestamp])
+        
 
-    def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        values = self.hash.get(key, [])
-
+    def get(self, key, timestamp):
         l = 0
+        values = self.hash.get(key, [])
         r = len(values) - 1
+        res = ""
 
         while(l <= r):
             mid = (l + r) // 2
-            if(timestamp >= values[mid][1]):
+            if(values[mid][1] <= timestamp):
                 res = values[mid][0]
                 l = mid + 1
             else:
                 r = mid - 1
+        
         return res
 
+        
 
 
 # Your TimeMap object will be instantiated and called as such:
