d7b2b25e89be9624f94bff8e6d287144179928c6
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-03 15:26:59 -0800

    LC-find-all-duplicates-in-an-array [Runtime: N/A; Memory: N/A]

--- hash-table/find-all-duplicates-in-an-array.py
+++ hash-table/find-all-duplicates-in-an-array.py
@@ -4,9 +4,6 @@ class Solution:
         arr = []
 
         for i in range(len(nums)):
-            nums[i] = abs(nums[i])
-
-        for i in range(len(nums)):
 
             if(-1 * nums[i] in nums):
                 arr.append(nums[i])
