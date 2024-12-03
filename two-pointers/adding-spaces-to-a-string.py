6fbcf044ce21cfc0f5dee1ecf17f78e428bb3aef
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-03 00:20:47 -0800

    LC-adding-spaces-to-a-string [Runtime: 47 ms; Memory: 49.2 MB]

--- two-pointers/adding-spaces-to-a-string.py
+++ two-pointers/adding-spaces-to-a-string.py
@@ -3,23 +3,30 @@ class Solution:
 
         res = ""
 
-        s_index = 0
-
         count = 0
 
-        while(s_index < len(spaces)):     
-
-            if(count == spaces[s_index]):
-                res += " "
-                s_index += 1
-
-            res += s[count]
-            
-            count += 1
+        for i in spaces:
+            res += s[count:i]
+            res += " "
+            count = i
         
         if count < len(s):
             res += s[count:]
+        
         return res
 
-
         
+
+        # while(s_index < len(spaces)):     
+
+        #     if(count == spaces[s_index]):
+        #         res += " "
+        #         s_index += 1
+
+        #     res += s[count]
+            
+        #     count += 1
+        
+        # if count < len(s):
+        #     res += s[count:]
+        # return res
