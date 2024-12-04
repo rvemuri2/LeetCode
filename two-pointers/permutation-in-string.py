eda3d8809325c87513d4cc3224245fa0f37abfef
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-04 14:58:12 -0800

    LC-permutation-in-string [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/permutation-in-string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        count_s1 = Counter(s1)
+
+        count_s2 = {}
+
+        L = 0
+
+
+        for R in range(len(s2)):
+
+            count_s2[s2[R]] = count_s2.get(s2[R], 0) + 1
+
+            if(R - L + 1 == len(s1)):
+
+                if(count_s1 == count_s2):
+                    return True
+                else:
+                    del count_s2[s2[L]]
+                    L += 1
+        
+        return False
+
+
+
+        
