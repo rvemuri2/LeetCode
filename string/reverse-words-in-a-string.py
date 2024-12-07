868380064b2db16b99d50e4bd207166065f10231
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-06 18:38:21 -0800

    LC-reverse-words-in-a-string [Runtime: 3 ms; Memory: 17.1 MB]

---
+++ string/reverse-words-in-a-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+
+        s = s.split()
+        print(s)
+
+        str1 = ""
+
+        arr = deque()
+
+        for i in range(len(s)):
+            if(i > 0):
+                arr.appendleft(s[i] + " ")
+            else:
+                arr.appendleft(s[i])
+        
+        for i in arr:
+            str1 += i
+        
+        
+        return str1
+        
