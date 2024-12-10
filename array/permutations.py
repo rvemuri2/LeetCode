361e6000bed6acc72b40398db56f4e311d4315a6
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-09 22:20:55 -0800

    LC-permutations [Runtime: 0 ms; Memory: 17.5 MB]

--- array/permutations.py
+++ array/permutations.py
@@ -1,21 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        if(len(nums) == 0):
-            return [[]]
-        
-        permutations = self.permute(nums[1:])
+        permutations = [[]]
 
-        res = []
-        for p in permutations:
-            for i in range(len(p) + 1):
-                p_copy = p.copy()
+        for n in nums:
+            new_permutations = []
+            for p in permutations:
+                for i in range(len(p) + 1):
+                    p_copy = p.copy()
+                    p_copy.insert(i, n)
+                    new_permutations.append(p_copy)
 
-                p_copy.insert(i, nums[0])
-
-                res.append(p_copy)
-
-        return res
+            permutations = new_permutations
+        return permutations
 
 
         
