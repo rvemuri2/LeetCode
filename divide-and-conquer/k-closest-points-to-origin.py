471c3a74b0164a5724803a774e931124ed848798
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-12 12:33:52 -0800

    LC-k-closest-points-to-origin [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/k-closest-points-to-origin.py
@@ -0,0 +1,35 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        graph = defaultdict(float)
+
+        heap = []
+
+        res = []
+
+        for i in points:
+
+            calc = math.pow(i[0], 2) + math.pow(i[1], 2)
+
+            val = math.sqrt(calc)
+
+            graph[tuple(i)] = val
+        
+        for key, val in graph.items():
+            if(len(heap) < k):
+                heapq.heappush(heap, (-val, key))
+            else:
+                heapq.heappushpop(heap, (-val, key))
+        
+        print(heap)
+        while(heap):
+            freq, element = heapq.heappop(heap)
+            res.append(element)
+
+        
+
+        print(graph)
+        return res
+        
+        
+        
