b43416bab59e5fec39012cf4f274a3c30b429928
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-14 18:38:31 -0800

    LC-minimum-average-of-smallest-and-largest-elements [Runtime: 4 ms; Memory: 17.3 MB]

--- two-pointers/minimum-average-of-smallest-and-largest-elements.py
+++ two-pointers/minimum-average-of-smallest-and-largest-elements.py
@@ -1,12 +1,12 @@
 class Solution:
-    def minimumAverage(self, nums: List[int]) -> float: # Or raise an exception if input is invalid
+    def minimumAverage(self, nums: List[int]) -> float:
         
         averages = []
-        nums = sorted(nums)  # Sort the list to make min/max access easy
+        nums = sorted(nums)
         
         while len(nums) > 1:
-            num1 = nums.pop(0)  # Smallest number
-            num2 = nums.pop(-1)  # Largest number
+            num1 = nums.pop(0)
+            num2 = nums.pop(-1)
             averages.append((num1 + num2) / 2)
         
         return min(averages)
