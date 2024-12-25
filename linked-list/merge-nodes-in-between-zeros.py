996b604b3cfb63a079887fbe50b35485e3aa0722
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-12-24 22:33:48 -0800

    LC-merge-nodes-in-between-zeros [Runtime: 289 ms; Memory: 82.3 MB]

--- linked-list/merge-nodes-in-between-zeros.py
+++ linked-list/merge-nodes-in-between-zeros.py
@@ -7,17 +7,19 @@ class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
         curr = head
+        tail = dummy = ListNode()
 
         while(curr.next):
-            node = curr = curr.next
+            node = ListNode(0)
             while(curr.next.val != 0):
                 node.val += curr.next.val
                 curr = curr.next
             
+            tail.next = node
+            tail = tail.next
             curr = curr.next
-            node.next = curr.next
         
-        return head.next
+        return dummy.next
 
 
 
