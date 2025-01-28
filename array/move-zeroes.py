ef0d8867372e87c33899a18e047558df845f7ee1
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-01-28 15:19:18 -0800

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- array/move-zeroes.py
+++ array/move-zeroes.py
@@ -4,15 +4,12 @@ class Solution:
         r = 1
 
         while(r < len(nums) and l < len(nums)):
-            if(nums[l] == 0 and nums[r] == 0):
-                r += 1
-                if(r >= len(nums)):
-                    break
-            
+
             if(nums[l] == 0 and nums[r] != 0):
                 nums[l], nums[r] = nums[r], nums[l]
-                r += 1
                 l += 1
+            
+            r+=1
         
         return nums
         
