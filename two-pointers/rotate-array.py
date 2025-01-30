8ba7342c95936a1eea6890606cd418698b26f89a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-01-29 23:07:11 -0800

    LC-rotate-array [Runtime: 1408 ms; Memory: 25.5 MB]

---
+++ two-pointers/rotate-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        
+        arr = []
+        m = k
+
+        while(k > 0):
+
+            var = nums[len(nums) - 1]
+            nums.insert(0, var)
+            nums.pop()
+            k -= 1
+        
