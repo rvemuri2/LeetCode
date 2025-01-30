4cdf670a3f3814d9eaeec26dd176b9d2601a3470
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-01-29 23:08:15 -0800

    LC-rotate-array [Runtime: 1404 ms; Memory: 25.6 MB]

--- math/rotate-array.py
+++ math/rotate-array.py
@@ -2,9 +2,8 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
 
         while(k > 0):
-
-            var = nums[len(nums) - 1]
-            nums.insert(0, var)
+            
+            nums.insert(0, nums[len(nums) - 1])
             nums.pop()
             k -= 1
         
