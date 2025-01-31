3597fa4d678509e9b3e67bd21d02d65496ac5c96
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-01-31 15:20:31 -0800

    LC-container-with-most-water [Runtime: 72 ms; Memory: 28.4 MB]

--- greedy/container-with-most-water.py
+++ greedy/container-with-most-water.py
@@ -3,12 +3,11 @@ class Solution:
 
         l = 0
         r = len(height) - 1
-
         area = 0
 
         while(l < r):
 
-            if(height[l] < height[r]):
+            if(height[r] > height[l]):
                 area = max(area, height[l] * (r - l))
                 l += 1
             
@@ -18,10 +17,9 @@ class Solution:
             
             if(height[r] == height[l]):
                 area = max(area, height[r] * (r - l))
-                l+=1
-                r-=1
-        
-        return area
-            
+                l += 1
+                r -= 1
 
-        
+        return area
+
+            
