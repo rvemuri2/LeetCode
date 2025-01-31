820c64d2fb03f067762a44c3f28508d991ce4258
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-01-31 15:01:08 -0800

    LC-two-sum-ii-input-array-is-sorted [Runtime: 8 ms; Memory: 18.5 MB]

--- binary-search/two-sum-ii-input-array-is-sorted.py
+++ binary-search/two-sum-ii-input-array-is-sorted.py
@@ -6,14 +6,14 @@ class Solution:
 
         while(l < r):
 
-            if(numbers[l] + numbers[r] > target):
-                r -= 1
-            
-            if(numbers[l] + numbers[r] < target):
-                l += 1
-            
             if(numbers[l] + numbers[r] == target):
                 return [l + 1, r + 1]
+
+            elif(numbers[l] + numbers[r] > target):
+                r -= 1
+            
+            else:
+                l += 1
         
 
         
