b14404c2b784258520ca642a3824e1ede80a0ffc
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-01 21:46:29 -0800

    LC-majority-element [Runtime: 3 ms; Memory: 19.2 MB]

--- divide-and-conquer/majority-element.py
+++ divide-and-conquer/majority-element.py
@@ -2,8 +2,16 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
         c = Counter(nums)
+        val = 0
+        k = 0
+        for key, value in c.items():
 
-        return max(c.keys())
+            if(value > val):
+                val = value
+                k = key
+
+
+        return k
 
         
         
