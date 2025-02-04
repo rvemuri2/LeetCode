0869fc60584398ee9b7998a7f363f2f39b837052
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-04 15:00:48 -0800

    LC-remove-duplicate-letters [Runtime: 2 ms; Memory: 18.1 MB]

---
+++ stack/remove-duplicate-letters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+
+        h = {}
+
+        for i in range(len(s)):
+            h[s[i]] = i
+        
+        arr = []
+        seen = set()
+
+        for i in range(len(s)):
+            if s[i] in seen:
+                continue
+            while(arr and arr[-1]>s[i] and h[arr[-1]] > i):
+                seen.remove(arr[-1])
+                arr.pop()
+
+            arr.append(s[i])
+            seen.add(s[i])
+        
+        return "".join(arr)
+            
+        
