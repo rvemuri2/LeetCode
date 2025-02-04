fdb85abaf8a9a357fa45df3b57d02378bd94cdbd
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-03 18:07:40 -0800

    LC-valid-parentheses [Runtime: 1 ms; Memory: 17.9 MB]

---
+++ stack/valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        h = {")": "(", "}": "{", "]": "["}
+
+        arr = []
+
+
+        for i in s:
+
+            if(len(arr) == 0 and i in h):
+                return False
+            
+            if(i == '(' or i == '[' or i == '{'):
+                arr.append(i)
+            
+            if(i in h):
+
+                if(len(arr) == 0):
+                    return False
+                
+                if(arr[-1] != h[i] and len(arr) > 0):
+                    return False
+                
+                if(arr[-1] == h[i]):
+                    arr.pop()
+        
+        return True if len(arr) == 0 else False
+
+
+        
