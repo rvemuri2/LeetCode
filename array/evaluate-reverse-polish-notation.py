74344f6642eeb7dec6748c57481a1bcbbca2a77d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-05 15:48:39 -0800

    LC-evaluate-reverse-polish-notation [Runtime: 2 ms; Memory: 19.1 MB]

--- array/evaluate-reverse-polish-notation.py
+++ array/evaluate-reverse-polish-notation.py
@@ -3,23 +3,26 @@ class Solution:
 
         arr = []
         
-        for i in tokens:
-
-            if(i == "+" or i == "*" or i == "-" or i == "/"):
-
-                num1 = arr.pop()
-                num2 = arr.pop()
+        for i in tokens:    
             
             if(i == "+"):
+                num1 = arr.pop()
+                num2 = arr.pop()
                 arr.append(num1 + num2)
             
             elif(i == "*"):
+                num1 = arr.pop()
+                num2 = arr.pop()
                 arr.append(num1 * num2)
             
             elif(i == "/"):
+                num1 = arr.pop()
+                num2 = arr.pop()
                 arr.append(int(num2 / num1))
             
             elif(i == "-"):
+                num1 = arr.pop()
+                num2 = arr.pop()
                 arr.append(num2 - num1)
             else:
                 arr.append(int(i))
