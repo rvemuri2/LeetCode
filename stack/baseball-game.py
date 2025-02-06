af0f787d7f2c6bedd98d1d02fa61e60e65cea754
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-06 15:21:33 -0800

    LC-baseball-game [Runtime: 0 ms; Memory: 18.2 MB]

--- stack/baseball-game.py
+++ stack/baseball-game.py
@@ -8,14 +8,14 @@ class Solution:
             if(i != "C" and i != "D" and i != "+"):
                 arr.append(int(i))
             
-            elif(i == "C"):
+            if(i == "C"):
                 arr.pop()
             
-            elif(i == "D"):
+            if(i == "D"):
                 num = arr[-1]
                 arr.append(num * 2)
             
-            elif(i == "+"):
+            if(i == "+"):
                 num = arr[-1]
                 num2 = arr[-2]
                 arr.append(num + num2)
