ad11de927df0912de39fde84c21b1d8a170607b7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-06 22:50:58 -0800

    LC-two-sum [Runtime: 0 ms; Memory: 18.8 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,16 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsList = {}
+        for i,n in enumerate(nums):
+            diff = target-n
+            if diff in numsList:
+                return [numsList[diff], i]
+            else:
+                numsList[n] = i
 
-        map1 = {}
-
-        for i, val in enumerate(nums):
-
-            if(target - val in map1):
-                return [map1[target - val], i]
-
-            
-            map1[val] = i
-
-            
 
         
