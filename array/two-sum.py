8cad13f0e69762acf260ca70b1feb75767ff13cb
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-06 22:51:28 -0800

    LC-two-sum [Runtime: 0 ms; Memory: 18.9 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -2,9 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numsList = {}
         for i,n in enumerate(nums):
-            diff = target-n
-            if diff in numsList:
-                return [numsList[diff], i]
+            if target - n in numsList:
+                return [numsList[target - n], i]
             else:
                 numsList[n] = i
 
