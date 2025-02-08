dc6af6bf309182fc9585adb3858a2aac648fc222
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-07 16:50:45 -0800

    LC-fizz-buzz [Runtime: 0 ms; Memory: 18.4 MB]

--- string/fizz-buzz.py
+++ string/fizz-buzz.py
@@ -2,18 +2,20 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
 
         arr = []
-        for i in range(1, n+1):
+        while(n > 0):
 
-            if(i % 3 == 0 and i % 5 == 0):
+            if(n % 3 == 0 and n % 5 == 0):
                 arr.append("FizzBuzz")
             
-            elif(i % 5 == 0):
+            elif(n % 5 == 0):
                 arr.append("Buzz")
             
-            elif(i % 3 == 0):
+            elif(n % 3 == 0):
                 arr.append("Fizz")
             
             else:
-                arr.append(str(i))
+                arr.append(str(n))
+            
+            n -= 1
         
-        return arr
+        return arr[::-1]
