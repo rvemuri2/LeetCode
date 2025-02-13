1a78e202459c3bcfa89e9d2a2e1771b50f6476b4
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-12 18:32:41 -0800

    LC-peak-index-in-a-mountain-array [Runtime: 0 ms; Memory: 29.5 MB]

--- binary-search/peak-index-in-a-mountain-array.py
+++ binary-search/peak-index-in-a-mountain-array.py
@@ -2,15 +2,15 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
 
         l, r = 0, len(arr) - 1
-        idx = float('-infinity')
+        idx = -1
     
         while(l <= r):
             mid = (l + r) // 2
-            if(arr[mid] >= arr[l]):
-                idx = max(idx, arr[mid])
-                l += 1
+            if(arr[mid] > arr[mid + 1]):
+                idx = mid
+                r = mid - 1
             else:
-                idx = max(idx, arr[mid]) 
-                r -= 1     
-        return arr.index(idx)
+                l = mid + 1
+
+        return idx
         
