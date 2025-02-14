c6861229cddd735715981a0693cc223d34fa91cb
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-13 21:54:48 -0800

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: 0 ms; Memory: 18.9 MB]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.py
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.py
@@ -14,7 +14,8 @@ class Solution:
             else:
                 left = mid + 1
     
-        left, right = 0, len(nums)-1
+        left = 0
+        right = len(nums)-1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
