0e75df6ca0cc8588d2d5880830ebd0d0aabfbe67
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-13 22:14:48 -0800

    LC-find-peak-element [Runtime: 0 ms; Memory: 17.9 MB]

--- binary-search/find-peak-element.py
+++ binary-search/find-peak-element.py
@@ -3,18 +3,17 @@ class Solution:
 
         l = 0
         r = len(nums) - 1
-        idx = 0
 
-        while(l < r):
+        while(l <= r):
 
             mid = (l + r) // 2
 
-            if(nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]):
-                idx = mid
+            if(mid > 0 and nums[mid] < nums[mid - 1]):
                 r = mid - 1
             
-            else:
+            elif(mid < len(nums) - 1 and nums[mid] < nums[mid + 1]):
                 l = mid + 1
-
-        return idx
+            
+            else:
+                return mid
         
