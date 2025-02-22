b252ba83435adc3c1b664eb00756c332115f5a87
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-21 22:39:43 -0800

    LC-binary-search [Runtime: 0 ms; Memory: 13.5 MB]

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,18 +1,27 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         l = 0
         r = len(nums) - 1
+        bool1 = False
 
         while(l <= r):
-            mid = (r + l) // 2
-            if target == nums[mid]:
-                return mid
-            elif target > nums[mid]:
-                l = mid + 1
-            else:
+
+            mid = (l + r) // 2
+
+            if(nums[mid] > target):
                 r = mid - 1
-        
-        return -1
             
-
-
+            elif(nums[mid] < target):
+                l = mid + 1
+            
+            else:
+                bool1 = True
+                return mid
+        
+        return mid if bool1 else -1 
+        
