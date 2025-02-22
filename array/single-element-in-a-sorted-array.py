8078723d1a92320c0339b83dc5f2814e20c889ae
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-21 23:58:04 -0800

    LC-single-element-in-a-sorted-array [Runtime: 0 ms; Memory: 25 MB]

---
+++ array/single-element-in-a-sorted-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+
+        l = 0
+        r = len(nums) - 1
+
+        while(l <= r):
+
+            mid = (l + r) // 2
+
+            if((mid - 1 < 0 or nums[mid] != nums[mid - 1]) and (mid + 1 == len(nums) or nums[mid] != nums[mid + 1])):
+                return nums[mid]
+            
+            left = mid - 1 if nums[mid - 1] == nums[mid] else mid
+            if(left % 2):
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        
