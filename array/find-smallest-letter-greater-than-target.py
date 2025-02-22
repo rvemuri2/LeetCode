6edbcd4792a696524120358efb1a3094e53a9bc2
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-21 22:57:31 -0800

    LC-find-smallest-letter-greater-than-target [Runtime: 0 ms; Memory: 13.7 MB]

--- array/find-smallest-letter-greater-than-target.py
+++ array/find-smallest-letter-greater-than-target.py
@@ -8,19 +8,19 @@ class Solution(object):
 
         l = 0
         r = len(letters) - 1
-        val = float('infinity')
+        val = letters[0]
 
         while(l <= r):
 
             mid = (l + r) // 2
 
             if(ord(letters[mid]) > ord(target)):
-                val = min(val, mid)
+                val = letters[mid]
                 r = mid - 1
 
             elif(ord(letters[mid]) <= ord(target)):
 
                 l = mid + 1
         
-        return letters[0] if val == float('infinity') else letters[val] 
+        return val
         
