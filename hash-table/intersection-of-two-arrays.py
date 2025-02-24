0b2833d687628d963df1c2e5867286191631e034
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-02-23 22:55:56 -0800

    LC-intersection-of-two-arrays [Runtime: 3 ms; Memory: 17.9 MB]

--- hash-table/intersection-of-two-arrays.py
+++ hash-table/intersection-of-two-arrays.py
@@ -1,10 +1,18 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-        set1 = set(nums1)
-        result = set()
-        for num in nums2:
-            if num in set1:
-                result.add(num)
-        return list(result)
+        nums1.sort()
+        nums2.sort()
+        i,j=0,0
+        a=[]
+        while i!=len(nums1) and j!=len(nums2):
+            if nums1[i]==nums2[j]:
+                a.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                i+=1
+        return list(set(a))
         
