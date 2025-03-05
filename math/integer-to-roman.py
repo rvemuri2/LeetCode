a99b4b3a99aa386e7fac6f2dc11328a242463564
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-03-04 23:46:13 -0800

    LC-integer-to-roman [Runtime: 8 ms; Memory: 17.8 MB]

--- math/integer-to-roman.py
+++ math/integer-to-roman.py
@@ -1,46 +1,23 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        d0 = {
-            0: "",
+        # Creating Dictionary for Lookup
+        num_map = {
             1: "I",
-            2: "II",
-            3: "III",
-            4: "IV",
-            5: "V",
-            6: "VI",
-            7: "VII",
-            8: "VIII",
-            9: "IX",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
         }
-        d3 = {
-            1: "M", 
-            2: "MM", 
-            3: "MMM", 
-            0: ""}
-        d2 = {
-            1: "C",
-            2: "CC",
-            3: "CCC",
-            4: "CD",
-            5: "D",
-            6: "DC",
-            7: "DCC",
-            8: "DCCC",
-            9: "CM",
-            0: "",
-        }
-        d1 = {
-            1: "X",
-            2: "XX",
-            3: "XXX",
-            4: "XL",
-            5: "L",
-            6: "LX",
-            7: "LXX",
-            8: "LXXX",
-            9: "XC",
-            0: "",
-        }
-        num = str(10000 + num)
-        return d3[int(num[1])] + d2[int(num[2])] + d1[int(num[3])] + d0[int(num[4])]
         
+        # Result Variable
+        r = ''
+        
+        
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            # If n in list then add the roman value to result variable
+            while n <= num:
+                r += num_map[n]
+                num-=n
+        return r
