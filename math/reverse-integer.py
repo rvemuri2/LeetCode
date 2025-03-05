7a905040dee6b3d32922164aa3cd4099daab4980
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-03-04 23:44:28 -0800

    LC-reverse-integer [Runtime: 32 ms; Memory: 17.8 MB]

--- math/reverse-integer.py
+++ math/reverse-integer.py
@@ -1,8 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
-
+        sum = 0
         sign = -1 if x < 0 else 1
-        rev = int(str(abs(x))[::-1]) * sign
-        return rev if -(2**31) <= rev <= (2**31 - 1) else 0
+        x = abs(x)
+
+        while x != 0:
+            rem = x % 10
+            sum = sum * 10 + rem
+            x = x // 10
+
+        sum *= sign
+        
+        # Check for overflow based on 32-bit integer limits
+        if sum > 2**31 - 1 or sum < -2**31:
+            return 0
+
+        return sum
 
         
