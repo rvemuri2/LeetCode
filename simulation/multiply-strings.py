7b958d4eac70850375418b1e3a525ce8983d458c
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-03-21 23:34:39 -0700

    LC-multiply-strings [Runtime: 1 ms; Memory: 17.6 MB]

--- simulation/multiply-strings.py
+++ simulation/multiply-strings.py
@@ -1,6 +1,7 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        n1, n2 = 0, 0
+        n1 = 0 
+        n2 = 0
         for i in range(len(num1)):
             n = ord(num1[i]) - 48
             n1 = n1*10 + n
