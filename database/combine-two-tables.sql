0ae996698223354aa9ba52d0dfb1a9aa72d6b253
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-04 23:41:54 -0700

    LC-combine-two-tables [Runtime: 407 ms; Memory: 0B]

--- database/combine-two-tables.sql
+++ database/combine-two-tables.sql
@@ -1,4 +1,2 @@
 -- SELECT Person.firstName, Person.lastName, Address.city, Address.state
--- FROM Person 
-
 select p.firstname,p.lastname,a.city,a.state from Person p left join Address a on p.personId=a.personId;
