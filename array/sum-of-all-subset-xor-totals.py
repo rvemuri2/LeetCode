15343bf06a5abd54e340e991e18ba852b067b348
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-04 23:38:46 -0700

    LC-sum-of-all-subset-xor-totals [Runtime: 0 ms; Memory: 17.6 MB]

--- array/sum-of-all-subset-xor-totals.py
+++ array/sum-of-all-subset-xor-totals.py
@@ -2,5 +2,5 @@ class Solution:
     def subsetXORSum(self, nums):
         total = 0
         for num in nums:
-            total |= num  # Step 1: Compute bitwise OR of all numbers
+            total |= num
         return total * (1 << (len(nums) - 1))
