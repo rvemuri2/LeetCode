14d6f9c58dec85ffefee8b56acd0c29a14da5a71
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-10 23:37:47 -0700

    LC-count-symmetric-integers [Runtime: 567 ms; Memory: 17.8 MB]

--- math/count-symmetric-integers.py
+++ math/count-symmetric-integers.py
@@ -3,7 +3,7 @@ class Solution:
         count = 0
         for num in range(low, high+1):
             s_num = str(num)
-            if len(s_num) & 1 == 0:         # or you can use
+            if len(s_num) & 1 == 0:         # or you can
                 mid = len(s_num) // 2
                 if sum(map(int, s_num[:mid])) == sum(map(int, s_num[mid:])):
                     count += 1
