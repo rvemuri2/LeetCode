d306dc803d300a1a5063f5d57f6c7874190cf882
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-10 23:35:39 -0700

    LC-count-symmetric-integers [Runtime: 571 ms; Memory: 17.6 MB]

--- enumeration/count-symmetric-integers.py
+++ enumeration/count-symmetric-integers.py
@@ -1,16 +1,10 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         count = 0
-        for i in range(low, high+1):
-            st = str(i) 
-            ln = len(st)
-            if ln%2!=0:
-                continue
-            mid = ln//2
-            first = st[mid:]
-            second = st[:mid]
-            left = sum(int(curr) for curr in first)
-            right = sum(int(curr) for curr in second)
-            if left == right:
-                count += 1
+        for num in range(low, high+1):
+            s_num = str(num)
+            if len(s_num) & 1 == 0:         # or you can use % operator to check len(s_num) is even or not 
+                mid = len(s_num) // 2
+                if sum(map(int, s_num[:mid])) == sum(map(int, s_num[mid:])):
+                    count += 1
         return count
