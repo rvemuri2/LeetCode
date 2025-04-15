96a1a855bbe1ff6fc4044dd9b7211d6e5ce18344
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-14 23:47:48 -0700

    LC-big-countries [Runtime: 259 ms; Memory: 0B]

--- database/big-countries.sql
+++ database/big-countries.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+# Write your MySQL query statement belo
 SELECT name, population, area
 FROM World
-WHERE area >= 3000000 OR population >= 24000000;
+WHERE area >= 3000000 OR population >= 25000000;
