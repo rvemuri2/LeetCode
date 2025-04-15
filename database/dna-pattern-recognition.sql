e6d3901e891d017af3f8c55dfa70307205132251
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-14 23:51:55 -0700

    LC-dna-pattern-recognition [Runtime: 342 ms; Memory: 0B]

--- database/dna-pattern-recognition.sql
+++ database/dna-pattern-recognition.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
-SELECT
-    sample_id,
-    dna_sequence,
-    species,
-    dna_sequence REGEXP '^ATG' AS has_start,
-    dna_sequence REGEXP 'TAA$|TAG$|TGA
-``` AS has_stop,
-    dna_sequence REGEXP 'ATAT' AS has_atat,
-    dna_sequence REGEXP 'GGG' AS has_ggg
-FROM samples
-ORDER BY sample_id;
+# Write your MySQL query statement below
+SELECT *,
+CASE WHEN dna_sequence LIKE 'ATG%' THEN 1 ELSE 0 END  has_start ,
+CASE WHEN dna_sequence LIKE '%TGA' OR dna_sequence LIKE '%TAG' OR dna_sequence LIKE '%TAA'THEN 1 ELSE 0 END  has_stop,
+CASE WHEN dna_sequence LIKE '%ATAT%' THEN 1 ELSE 0 END  has_atat,
+CASE WHEN dna_sequence LIKE '%GGG%' THEN 1 ELSE 0 END  has_ggg
+FROM Samples
