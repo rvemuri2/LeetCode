aa88a28d012076e32b6b79a78cde1055bed7d2df
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-19 11:09:02 -0700

    LC-next-permutation [Runtime: 0 ms; Memory: 17.9 MB]

--- array/next-permutation.py
+++ array/next-permutation.py
@@ -9,7 +9,7 @@ class Solution:
             if nums[i]<nums[i+1]:
                 pivot=i
                 break
-        if pivot==-1: #if given permutation is lexi last
+        if pivot==-1: #if given permutation is lexi
             nums.reverse()
             return
         for j in range(n-1,pivot,-1):
