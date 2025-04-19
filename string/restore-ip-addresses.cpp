c3825f615a420f89d1434c0d497b06f50e5ae1c9
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-19 11:12:10 -0700

    LC-restore-ip-addresses [Runtime: 0 ms; Memory: 10.7 MB]

--- string/restore-ip-addresses.cpp
+++ string/restore-ip-addresses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // Helper function to check if a given string is a valid IP address segment
     bool valid(string temp){
-        if(temp.size()>3 || temp.size()==0) return false; // segment length should be between 1 and 3
+        if(temp.size()>3 || temp.size()==0) return false;
         if(temp.size()>1 && temp[0]=='0')   return false;
         if(temp.size() && stoi(temp)>255) return false;
         return true;
