892112c45cb0d45d42fc30b53697e9761d8ce566
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-19 11:08:30 -0700

    LC-next-permutation [Runtime: 0 ms; Memory: 17.8 MB]

---
+++ array/next-permutation.py
@@ -0,0 +1,25 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        pivot=-1
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                pivot=i
+                break
+        if pivot==-1: #if given permutation is lexi last, return first
+            nums.reverse()
+            return
+        for j in range(n-1,pivot,-1):
+            if nums[j]>nums[pivot]:
+                nums[j], nums[pivot]=nums[pivot], nums[j]
+                break
+        l, r = pivot+1, n-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l+=1
+            r-=1
+
+        
