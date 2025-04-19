ace2551f365f2e928cde48b228ef748e133b148d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-19 11:10:05 -0700

    LC-next-permutation [Runtime: 0 ms; Memory: 17.7 MB]

--- array/next-permutation.py
+++ array/next-permutation.py
@@ -1,8 +1,6 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        
         n=len(nums)
         pivot=-1
         for i in range(n-2,-1,-1):
