1848d7fa99849d3784417de02c9d484d8ffdc082
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-21 23:38:31 -0700

    LC-string-to-integer-atoi [Runtime: 0 ms; Memory: 20.8 MB]

--- string/string-to-integer-atoi.php
+++ string/string-to-integer-atoi.php
@@ -6,7 +6,9 @@ class Solution {
      */
     function myAtoi($s) {
         $a = (int)str_replace('e', 'a', $s);
-        if ($a > 2**31 - 1) return 2**31 - 1;
+        if ($a > 2**31 - 1) {
+            return 2**31 - 1;
+        } 
         if ($a < -2**31) return -2**31;
         return $a;
     }
