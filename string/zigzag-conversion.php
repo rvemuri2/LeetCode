e301cdf9a217dd34b8f70fe2ccb34bc79e63ce91
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-21 23:36:40 -0700

    LC-zigzag-conversion [Runtime: 4 ms; Memory: 20.7 MB]

--- string/zigzag-conversion.php
+++ string/zigzag-conversion.php
@@ -7,7 +7,7 @@ class Solution {
      */
     function convert($s, $numRows)
     {
-        // If numRows is 1 or greater than or equal to the length of the string, return the string itself
+        // If numRows is 1 or greater than or equal to the length of the string
         if ($numRows <= 1 || $numRows >= strlen($s)) {
             return $s;
         }
