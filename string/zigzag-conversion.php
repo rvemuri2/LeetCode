5fc12897f683a91c66b7b2f8e139def7f396ab73
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-21 23:33:56 -0700

    LC-zigzag-conversion [Runtime: 4 ms; Memory: 20.8 MB]

---
+++ string/zigzag-conversion.php
@@ -0,0 +1,36 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
+     */
+    function convert($s, $numRows)
+    {
+        // If numRows is 1 or greater than or equal to the length of the string, return the string itself
+        if ($numRows <= 1 || $numRows >= strlen($s)) {
+            return $s;
+        }
+
+        // Create an array to hold the strings for each row
+        $rows = array_fill(0, $numRows, '');
+        $curRow = 0;
+        $goingDown = false;
+
+        // Iterate through each character in the string
+        for ($i = 0; $i < strlen($s); $i++) {
+            $rows[$curRow] .= $s[$i]; // Append the character to the current row
+            // Change direction if we hit the top or bottom row
+            if ($curRow == 0) {
+                $goingDown = true;
+            } elseif ($curRow == $numRows - 1) {
+                $goingDown = false;
+            }
+            // Move to the next row
+            $curRow += $goingDown ? 1 : -1;
+        }
+
+        // Combine all rows into a single string
+        return implode('', $rows);
+    }
+}
