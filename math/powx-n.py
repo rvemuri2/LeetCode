b5a5bdf1f3730b8de150f7065f4b3321a97569a3
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-22 23:45:03 -0700

    LC-powx-n [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ math/powx-n.py
@@ -0,0 +1,23 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+         # Handle the case when n is negative by converting it to positive
+    # and inverting x
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1
+        current_product = x
+
+    # Use exponentiation by squaring for efficiency (log(n) time)
+        while n > 0:
+        # If the current bit is 1, multiply result by current_product
+            if n % 2 == 1:
+                result *= current_product
+
+        # Square the base and shift exponent right
+            current_product *= current_product
+            n //= 2
+
+        return result
+        
