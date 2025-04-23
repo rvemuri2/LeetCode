477a027c389280fdfefee808a7476a1babdd16fb
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-22 23:36:02 -0700

    LC-count-largest-group [Runtime: 42 ms; Memory: 17.7 MB]

--- hash-table/count-largest-group.py
+++ hash-table/count-largest-group.py
@@ -1,4 +1,3 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        # Solution 2: one-liner
         return list((c := Counter(sum(map(int, str(i))) for i in range(1, n+1))).values()).count(max(c.values()))
