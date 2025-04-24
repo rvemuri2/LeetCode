482f678809f69976c18b7c6ea04c5309b8112df6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-23 23:44:49 -0700

    LC-patients-with-a-condition [Runtime: 379 ms; Memory: 0B]

--- database/patients-with-a-condition.sql
+++ database/patients-with-a-condition.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
 
 select patient_id,patient_name,conditions from Patients
 where conditions like 'DIAB1%'  or  conditions like '% DIAB1%' ;
