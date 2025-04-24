971ecab6a63817b3e368f9395d9f9fa1d40ea370
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-23 23:38:13 -0700

    LC-count-complete-subarrays-in-an-array [Runtime: 1471 ms; Memory: 18 MB]

--- array/count-complete-subarrays-in-an-array.py
+++ array/count-complete-subarrays-in-an-array.py
@@ -1,22 +1,19 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        # Step 1
+
         total_distinct = len(set(nums))
         count = 0
         n = len(nums)
 
-    # Step 2
         for i in range(n):
             freq = defaultdict(int)
             distinct = 0
 
-        # Expand the subarray
             for j in range(i, n):
                 if freq[nums[j]] == 0:
                     distinct += 1
                 freq[nums[j]] += 1
 
-            # Step 3
                 if distinct == total_distinct:
                     count += 1
 
