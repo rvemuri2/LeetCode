b9845261b0af0251c33f9dbe4bd917d0452a4a00
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-25 23:10:46 -0700

    LC-remove-nth-node-from-end-of-list [Runtime: 0 ms; Memory: 17.7 MB]

--- linked-list/remove-nth-node-from-end-of-list.py
+++ linked-list/remove-nth-node-from-end-of-list.py
@@ -7,8 +7,7 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)  # Dummy node simplifies edge cases
         first = second = dummy
-
-    # Move first pointer n+1 steps ahead
+        
         for _ in range(n + 1):
             first = first.next
 
