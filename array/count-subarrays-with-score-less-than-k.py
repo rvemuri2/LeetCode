5499e90700e18307e546f18f95eb27c01f4969ab
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-27 23:44:15 -0700

    LC-count-subarrays-with-score-less-than-k [Runtime: 119 ms; Memory: 30.7 MB]

--- array/count-subarrays-with-score-less-than-k.py
+++ array/count-subarrays-with-score-less-than-k.py
@@ -1,14 +1,13 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        left = 0
+        n, left = len(nums), 0
         total = 0
         current_sum = 0
         count = 0
 
         for right in range(n):
             current_sum += nums[right]
-            
+
             while left <= right and current_sum * (right - left + 1) >= k:
                 current_sum -= nums[left]
                 left += 1
