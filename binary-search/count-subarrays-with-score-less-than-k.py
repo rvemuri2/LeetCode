3fd42380b1c93d5c5006b13498ab810b97da5ba2
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-27 23:43:11 -0700

    LC-count-subarrays-with-score-less-than-k [Runtime: 114 ms; Memory: 30.4 MB]

---
+++ binary-search/count-subarrays-with-score-less-than-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        left = 0
+        total = 0
+        current_sum = 0
+        count = 0
+
+        for right in range(n):
+            current_sum += nums[right]
+
+        # While score >= k, shrink window from the left
+            while left <= right and current_sum * (right - left + 1) >= k:
+                current_sum -= nums[left]
+                left += 1
+
+        # All subarrays ending at `right` and starting between [left, right] are valid
+            count += right - left + 1
+
+        return count
+        
