f9a2c4a7c352574d28db4544f4f6b1e2a172f6c6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-04-28 20:27:53 -0700

    LC-employee-bonus [Runtime: 999 ms; Memory: 0B]

--- database/employee-bonus.sql
+++ database/employee-bonus.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
 SELECT Employee.name,Bonus.bonus FROM Employee 
 LEFT JOIN Bonus ON Employee.empID = Bonus.empID
 WHERE bonus < 1000 OR Bonus IS NULL ;
