e8ba4a8f081d93248ff8292a4535e65480d2a835
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-01 23:56:42 -0700

    LC-build-array-from-permutation [Runtime: 0 ms; Memory: 17.7 MB]

--- array/build-array-from-permutation.py
+++ array/build-array-from-permutation.py
@@ -2,7 +2,6 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        # Initialize an empty list to store the result
         ans = []
 
         for i in range(len(nums)):
