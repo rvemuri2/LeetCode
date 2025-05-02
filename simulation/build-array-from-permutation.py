a5451b152db53bb877457cc9aea190dd74c4f508
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-01 23:56:12 -0700

    LC-build-array-from-permutation [Runtime: 0 ms; Memory: 17.9 MB]

--- simulation/build-array-from-permutation.py
+++ simulation/build-array-from-permutation.py
@@ -8,7 +8,7 @@ class Solution:
         # Iterate over each index in the input list
         for i in range(len(nums)):
             # For each index i, append nums[nums[i]] to the result list
-            value = nums[nums[i]]  # First get the value at index nums[i]
+            value = nums[nums[i]]  
             ans.append(value)
 
         return ans
