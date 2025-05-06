6f51904485624f7e95097a47ec5907e047b4465d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-05 23:34:35 -0700

    LC-insert-interval [Runtime: 1 ms; Memory: 44.4 MB]

--- array/insert-interval.java
+++ array/insert-interval.java
@@ -2,12 +2,7 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[][] intervals1 = {{1, 3}, {6, 9}};
-        int[] newInterval1 = {2, 5};
-        System.out.println(Arrays.deepToString(sol.insert(intervals1, newInterval1)));
-        int[][] intervals2 = {{1,2}, {3,5}, {6,7}, {8,10}, {12,16}};
-        int[] newInterval2 = {4,8};
+       
     }
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
