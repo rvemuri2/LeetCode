74ad16dc9f713ec3c8ed464d4ca2848cd06f3cea
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-05 23:28:39 -0700

    LC-build-array-from-permutation [Runtime: 1 ms; Memory: 45.4 MB]

--- simulation/build-array-from-permutation.java
+++ simulation/build-array-from-permutation.java
@@ -13,7 +13,7 @@ public class Solution {
 
     public int[] buildArray(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n]; // Result array
+        int[] ans = new int[n];
 
         for (int i = 0; i < n; i++) {
             ans[i] = nums[nums[i]];
