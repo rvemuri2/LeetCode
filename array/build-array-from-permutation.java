d637a818c9a7bf03aeb35ff27ea3d27f3c8ddc2b
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-05 23:27:34 -0700

    LC-build-array-from-permutation [Runtime: 1 ms; Memory: 45.6 MB]

---
+++ array/build-array-from-permutation.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {0, 2, 1, 5, 3, 4};
+        int[] nums2 = {5, 0, 1, 2, 3, 4};
+
+        System.out.println(Arrays.toString(sol.buildArray(nums1))); // Output: [0, 1, 2, 4, 5, 3]
+        System.out.println(Arrays.toString(sol.buildArray(nums2))); // Output: [4, 5, 0, 1, 2, 3]
+    }
+
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n]; // Result array
+
+        // Loop through each index i
+        for (int i = 0; i < n; i++) {
+            // Set ans[i] to nums[nums[i]]
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
