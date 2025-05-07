1c007fd060850fdc6917bb5858214aeb37d00ca7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-06 21:36:11 -0700

    LC-pascals-triangle [Runtime: 1 ms; Memory: 41.5 MB]

---
+++ dynamic-programming/pascals-triangle.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.generate(5));
+        System.out.println(sol.generate(1));
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        // Build each row one by one
+        for (int row = 0; row < numRows; row++) {
+            List<Integer> currentRow = new ArrayList<>();
+
+            // First and last elements of each row are always 1
+            currentRow.add(1);
+
+            // Fill in the inner elements of the row (if row >= 2)
+            for (int col = 1; col < row; col++) {
+                int prevRowLeft = triangle.get(row - 1).get(col - 1);
+                int prevRowRight = triangle.get(row - 1).get(col);
+                currentRow.add(prevRowLeft + prevRowRight);
+            }
+
+            // Add the last 1 if this is not the first row
+            if (row > 0) {
+                currentRow.add(1);
+            }
+
+            // Add this row to the triangle
+            triangle.add(currentRow);
+        }
+
+        return triangle;
+    }
+}
