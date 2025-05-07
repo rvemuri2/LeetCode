22c99322a7f84bf35a972408ece9d0970edd4ff0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-06 21:33:25 -0700

    LC-excel-sheet-column-title [Runtime: 0 ms; Memory: 40.7 MB]

--- math/excel-sheet-column-title.java
+++ math/excel-sheet-column-title.java
@@ -1,6 +1,4 @@
 public class Solution {
-    public static void main(String[] args) {
-    }
 
     public String convertToTitle(int columnNumber) {
         StringBuilder result = new StringBuilder();
