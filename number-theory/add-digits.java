f25d0ba003e14c6ceaa3f26b1ca92ff5467d0caf
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-07 23:00:02 -0700

    LC-add-digits [Runtime: 0 ms; Memory: 41 MB]

--- number-theory/add-digits.java
+++ number-theory/add-digits.java
@@ -1,12 +1,4 @@
 public class Solution {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        System.out.println(sol.addDigits(38)); // Output: 2
-        System.out.println(sol.addDigits(0));  // Output: 0
-        System.out.println(sol.addDigits(99)); // Output: 9
-    }
-
     public int addDigits(int num) {
         if (num == 0) return 0;
         return 1 + (num - 1) % 9;
