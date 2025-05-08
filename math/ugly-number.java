5cedb6d7d7cdfec32161859b1d191b9b450b4bc1
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-07 22:57:47 -0700

    LC-ugly-number [Runtime: 1 ms; Memory: 40.8 MB]

--- math/ugly-number.java
+++ math/ugly-number.java
@@ -1,7 +1,5 @@
 public class Solution {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-    }
+    public static void main(String[] args) {}
     public boolean isUgly(int n) {
         if (n <= 0) return false;
         for (int factor : new int[]{2, 3, 5}) {
