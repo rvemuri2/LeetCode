6bd58415ed96109c23539a64524a38ec12f63686
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-08 23:45:40 -0700

    LC-edit-distance [Runtime: 5 ms; Memory: 45.1 MB]

--- dynamic-programming/edit-distance.java
+++ dynamic-programming/edit-distance.java
@@ -14,7 +14,7 @@ public class Solution {
         int[][] dp = new int[m + 1][n + 1];
 
         // Initialize base cases
-        for (int i = 0; i <= m; i++) dp[i][0] = i; // Deleting all characters
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
         for (int j = 0; j <= n; j++) dp[0][j] = j;
 
         for (int i = 1; i <= m; i++) {
