5dc0245bb444600d885d7649cdc2c43c83e8c7e1
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-09 23:50:52 -0700

    LC-valid-sudoku [Runtime: 2 ms; Memory: 44.5 MB]

--- matrix/valid-sudoku.java
+++ matrix/valid-sudoku.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         char[][] board = {
-            {'5','3','.','.','7','.','.','.','.'},
         };
 
         Solution sol = new Solution();
