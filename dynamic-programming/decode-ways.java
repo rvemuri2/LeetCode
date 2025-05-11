a00c9bb9511fda6304522db1d04aa9fb46fad2c6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-10 23:01:59 -0700

    LC-decode-ways [Runtime: 1 ms; Memory: 41.8 MB]

---
+++ dynamic-programming/decode-ways.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
+
+        int n = s.length();
+        int[] dp = new int[n + 1]; // dp[i] = # of ways to decode up to i chars
+        dp[0] = 1; // empty string
+        dp[1] = 1; // first char is guaranteed non-zero by the check above
+
+        for (int i = 2; i <= n; i++) {
+            // Single digit
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // Two digits
+            int twoDigit = Integer.parseInt(s.substring(i - 2, i));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
