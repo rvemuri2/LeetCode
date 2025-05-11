105fa2dc07ea45f9c4ee910122e609fa5921187e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-10 23:03:56 -0700

    LC-decode-ways [Runtime: 1 ms; Memory: 41.9 MB]

--- string/decode-ways.java
+++ string/decode-ways.java
@@ -1,6 +1,8 @@
 public class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+            return 0;
+        }
 
         int n = s.length();
         int[] dp = new int[n + 1];
