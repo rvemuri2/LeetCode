3ea6af9c5421d93cb8df532f40a0f51d763db7ad
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-10 23:05:56 -0700

    LC-simplify-path [Runtime: 4 ms; Memory: 43.4 MB]

---
+++ stack/simplify-path.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        Iterator<String> it = stack.descendingIterator();
+        while (it.hasNext()) {
+            result.append("/").append(it.next());
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
+    }
+
+    // Test driver
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String[] testPaths = {
+            "/home/",
+            "/../",
+            "/home//foo/",
+            "/a/./b/../../c/",
+            "/.../a/../b",
+            "/home/user/Documents/../Pictures",
+            "/a/b/c/../../../..",
+            "/a/../../b/../c//.//"
+        };
+
+        for (String path : testPaths) {
+            System.out.println("Input:    " + path);
+            System.out.println("Output:   " + sol.simplifyPath(path));
+            System.out.println("----------");
+        }
+    }
+}
