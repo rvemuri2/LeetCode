4e0b70ca6c443204f9348b9f560a6a298f12b31e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-11 22:04:25 -0700

    LC-jump-game [Runtime: 3 ms; Memory: 45.9 MB]

---
+++ dynamic-programming/jump-game.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;  // Furthest index we can currently reach
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reachable) {
+                // If the current index is beyond the furthest we can reach
+                return false;
+            }
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+
+        return true; // We made it through the array
+    }
+
+    // Test driver
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.canJump(new int[]{2, 3, 1, 1, 4})); // true
+        System.out.println(sol.canJump(new int[]{3, 2, 1, 0, 4})); // false
+        System.out.println(sol.canJump(new int[]{0}));             // true
+        System.out.println(sol.canJump(new int[]{2, 0, 0}));       // true
+    }
+}
