8201b0807da5f56004f4e8d977e3dea5871d983b
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-11 22:06:18 -0700

    LC-jump-game [Runtime: 3 ms; Memory: 45.7 MB]

--- greedy/jump-game.java
+++ greedy/jump-game.java
@@ -1,10 +1,8 @@
 public class Solution {
     public boolean canJump(int[] nums) {
         int reachable = 0;  // Furthest index we can currently reach
-
         for (int i = 0; i < nums.length; i++) {
             if (i > reachable) {
-                // If the current index is beyond the furthest we can reach
                 return false;
             }
             reachable = Math.max(reachable, i + nums[i]);
