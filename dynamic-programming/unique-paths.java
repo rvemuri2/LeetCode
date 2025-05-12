7932af35126c7cf19d4df3c2a89e521a11360119
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-11 22:10:43 -0700

    LC-unique-paths [Runtime: 0 ms; Memory: 40.6 MB]

--- dynamic-programming/unique-paths.java
+++ dynamic-programming/unique-paths.java
@@ -1,6 +1,5 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        // We calculate C(m + n - 2, m - 1) or C(m + n - 2, n - 1)
         int N = m + n - 2;
         int k = Math.min(m - 1, n - 1);
         long res = 1;
