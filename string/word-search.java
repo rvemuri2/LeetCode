2726551bd02a301fc09d0a224172e42738de2126
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-12 23:29:12 -0700

    LC-word-search [Runtime: 128 ms; Memory: 41.6 MB]

--- string/word-search.java
+++ string/word-search.java
@@ -45,13 +45,10 @@ public class Solution {
     // Test driver
     public static void main(String[] args) {
         Solution sol = new Solution();
-
         char[][] board = {
             {'A','B','C','E'},
             {'S','F','C','S'},
             {'A','D','E','E'}
         };
-
-        System.out.println(sol.exist(board, "ABCCED")); // true
     }
 }
