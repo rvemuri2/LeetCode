758b05834d6c7b1a84bbecf6057625f3965e3449
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-13 17:39:15 -0700

    LC-gas-station [Runtime: 2 ms; Memory: 56.6 MB]

---
+++ greedy/gas-station.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int currentGas = 0;
+        int startIndex = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int gain = gas[i] - cost[i];
+            totalGas += gain;
+            currentGas += gain;
+
+            // If currentGas drops below 0, we cannot start from current segment
+            if (currentGas < 0) {
+                startIndex = i + 1;
+                currentGas = 0;
+            }
+        }
+
+        // If totalGas is negative, we can't complete the circuit
+        return totalGas >= 0 ? startIndex : -1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.canCompleteCircuit(
+            new int[]{1,2,3,4,5},
+            new int[]{3,4,5,1,2}
+        )); // Output: 3
+
+        System.out.println(sol.canCompleteCircuit(
+            new int[]{2,3,4},
+            new int[]{3,4,3}
+        )); // Output: -1
+    }
+}
