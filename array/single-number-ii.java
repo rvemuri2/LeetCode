253b04425791fb3a26e2005e2c2894d0cfa331f8
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-13 17:48:52 -0700

    LC-single-number-ii [Runtime: 3 ms; Memory: 46 MB]

--- array/single-number-ii.java
+++ array/single-number-ii.java
@@ -25,6 +25,6 @@ public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.singleNumber(new int[]{2, 2, 3, 2}));        // Output: 3
-        System.out.println(sol.singleNumber(new int[]{0, 1, 0, 1, 0, 1, 99})); // Output: 99
+        System.out.println(sol.singleNumber(new int[]{0, 1, 0, 1, 0, 1, 99}));
     }
 }
