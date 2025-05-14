fcde43be6e9ab6e825cbb7acdb7727a284e9f49f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-13 17:48:39 -0700

    LC-single-number-ii [Runtime: 3 ms; Memory: 45.3 MB]

---
+++ bit-manipulation/single-number-ii.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        // Check each bit position
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            int mask = 1 << i;
+
+            for (int num : nums) {
+                if ((num & mask) != 0) {
+                    sum++;
+                }
+            }
+
+            // If sum % 3 != 0, the unique number has a 1 in this bit position
+            if (sum % 3 != 0) {
+                result |= mask;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.singleNumber(new int[]{2, 2, 3, 2}));        // Output: 3
+        System.out.println(sol.singleNumber(new int[]{0, 1, 0, 1, 0, 1, 99})); // Output: 99
+    }
+}
