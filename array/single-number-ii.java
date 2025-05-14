fa7152959071257af8c5bd78c0438bb88ffc7ad5
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-13 17:49:41 -0700

    LC-single-number-ii [Runtime: 4 ms; Memory: 45.6 MB]

--- array/single-number-ii.java
+++ array/single-number-ii.java
@@ -1,12 +1,9 @@
 public class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-
-        // Check each bit position
         for (int i = 0; i < 32; i++) {
             int sum = 0;
             int mask = 1 << i;
-
             for (int num : nums) {
                 if ((num & mask) != 0) {
                     sum++;
