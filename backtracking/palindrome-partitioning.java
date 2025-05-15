bae408850979fea0abf97869f66456a2a71e5353
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-14 22:22:58 -0700

    LC-palindrome-partitioning [Runtime: 7 ms; Memory: 57.3 MB]

--- backtracking/palindrome-partitioning.java
+++ backtracking/palindrome-partitioning.java
@@ -26,7 +26,9 @@ public class Solution {
     private boolean isPalindrome(String s) {
         int l = 0, r = s.length() - 1;
         while (l < r) {
-            if (s.charAt(l++) != s.charAt(r--)) return false;
+            if (s.charAt(l++) != s.charAt(r--)) {
+                return false;
+            }
         }
         return true;
     }
