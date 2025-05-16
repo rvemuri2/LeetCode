e736e2b473906b28118e2ff10e62f4c0936179dd
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-15 23:53:35 -0700

    LC-longest-consecutive-sequence [Runtime: 30 ms; Memory: 68 MB]

--- array/longest-consecutive-sequence.java
+++ array/longest-consecutive-sequence.java
@@ -38,9 +38,8 @@ public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        // Test cases
         int[] nums1 = {100, 4, 200, 1, 3, 2};
-        System.out.println("Longest sequence length: " + sol.longestConsecutive(nums1)); // Output: 4
+        System.out.println("Longest sequence length: " + sol.longestConsecutive(nums1));
 
     }
 }
