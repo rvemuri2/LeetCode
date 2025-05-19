dd89ea2f8bdbb404bd5680570c2fbd1f84838283
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-18 21:14:50 -0700

    LC-type-of-triangle [Runtime: 0 ms; Memory: 42.1 MB]

--- sorting/type-of-triangle.java
+++ sorting/type-of-triangle.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String triangleType(int[] nums) {
-        int a = nums[0]; 
+        int a = nums[0]; //1
         int b = nums[1];
         int c = nums[2];
         if (a + b <= c || a + c <= b || b + c <= a) {
