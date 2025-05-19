67ec051f2d7628fa9a58f442d58ea0d2d9a22d28
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-18 21:11:55 -0700

    LC-type-of-triangle [Runtime: 0 ms; Memory: 41.8 MB]

---
+++ array/type-of-triangle.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public String triangleType(int[] nums) {
+        int a = nums[0], b = nums[1], c = nums[2];
+
+        // Check for triangle inequality
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return "none";
+        }
+
+        // Check triangle type
+        if (a == b && b == c) {
+            return "equilateral";
+        } else if (a == b || b == c || a == c) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.triangleType(new int[]{3, 3, 3})); // equilateral
+        System.out.println(sol.triangleType(new int[]{3, 4, 5})); // scalene
+        System.out.println(sol.triangleType(new int[]{3, 3, 5})); // isosceles
+        System.out.println(sol.triangleType(new int[]{1, 2, 3})); // none
+    }
+}
