77401a1b8f5bcdffcd74fb3df36de7840c0433cd
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-18 21:14:04 -0700

    LC-type-of-triangle [Runtime: 0 ms; Memory: 42.2 MB]

--- math/type-of-triangle.java
+++ math/type-of-triangle.java
@@ -1,6 +1,7 @@
 public class Solution {
     public String triangleType(int[] nums) {
-        int a = nums[0], b = nums[1], c = nums[2];
+        int a = nums[0]; 
+        int b = nums[1], c = nums[2];
         if (a + b <= c || a + c <= b || b + c <= a) {
             return "none";
         }
