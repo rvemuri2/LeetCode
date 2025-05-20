53817a9370124c502594551f25f0583f3d5a1516
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-19 23:25:55 -0700

    LC-zero-array-transformation-i [Runtime: 3 ms; Memory: 97.2 MB]

--- prefix-sum/zero-array-transformation-i.java
+++ prefix-sum/zero-array-transformation-i.java
@@ -47,6 +47,5 @@ public class Solution {
         Solution sol = new Solution();
         int[] nums1 = {1, 0, 1};
         int[][] queries1 = {{0, 2}};
-        System.out.println(sol.isZeroArray(nums1, queries1));
     }
 }
