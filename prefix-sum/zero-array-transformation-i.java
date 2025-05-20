6c3bc1fc1b58c97c9db7301618be6bd929b06871
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-19 23:28:24 -0700

    LC-zero-array-transformation-i [Runtime: 3 ms; Memory: 96.9 MB]

--- prefix-sum/zero-array-transformation-i.java
+++ prefix-sum/zero-array-transformation-i.java
@@ -4,8 +4,6 @@ public class Solution {
      * Determines if it is possible to transform the given nums array into a zero array
      * using the provided range queries. Each query allows decrementing any subset of
      * values within the specified range by 1.
-     *
-     * @param nums    The initial array of non-negative integers.
      */
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
