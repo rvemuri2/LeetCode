6e7b6988d3bc6335a8b8d32e0b1432dba20b08f6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-20 23:40:44 -0700

    LC-set-matrix-zeroes [Runtime: 1 ms; Memory: 45.7 MB]

--- array/set-matrix-zeroes.java
+++ array/set-matrix-zeroes.java
@@ -61,7 +61,6 @@ public class Solution {
             {1, 1, 1}
         };
         sol.setZeroes(matrix1);
-        printMatrix(matrix1);
     }
 
     private static void printMatrix(int[][] matrix) {
