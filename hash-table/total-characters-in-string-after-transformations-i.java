2af4fd01acee3285fff42496758e1dd1c38de68a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-21 17:54:06 -0700

    LC-total-characters-in-string-after-transformations-i [Runtime: 250 ms; Memory: 45.4 MB]

--- hash-table/total-characters-in-string-after-transformations-i.java
+++ hash-table/total-characters-in-string-after-transformations-i.java
@@ -3,7 +3,7 @@ public class Solution {
 
     // Method to compute the length of the string after t transformations
     public int lengthAfterTransformations(String s, int t) {
-        long[] count = new long[26]; // Frequency count for characters 'a' to 'z'
+        long[] count = new long[26];
         for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
