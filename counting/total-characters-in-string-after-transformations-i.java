4b0384c7461ba4e026c013c4ff8838f944a6a556
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-21 17:51:31 -0700

    LC-total-characters-in-string-after-transformations-i [Runtime: 257 ms; Memory: 45.1 MB]

--- counting/total-characters-in-string-after-transformations-i.java
+++ counting/total-characters-in-string-after-transformations-i.java
@@ -39,6 +39,5 @@ public class Solution {
     }
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.lengthAfterTransformations("abcyy", 2));
     }
 }
