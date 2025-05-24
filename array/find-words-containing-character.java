a38edd136053a4efa6f86b566655572146805665
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-23 23:27:34 -0700

    LC-find-words-containing-character [Runtime: 1 ms; Memory: 45.2 MB]

--- array/find-words-containing-character.java
+++ array/find-words-containing-character.java
@@ -28,10 +28,9 @@ class Solution {
     // Main method to test the functionality with example inputs
     public static void main(String[] args) {
         Solution sol = new Solution();
-
         // Test case 1: both words contain 'e'
         System.out.println(sol.findWordsContaining(new String[]{"leet", "code"}, 'e')); // Output: [0, 1]
-        // Test case 2: 'a' is in "abc" and "aaaa"
+
         System.out.println(sol.findWordsContaining(new String[]{"abc", "bcd", "aaaa", "cbc"}, 'a'));
 
         System.out.println(sol.findWordsContaining(new String[]{"abc", "bcd", "aaaa", "cbc"}, 'z'));
