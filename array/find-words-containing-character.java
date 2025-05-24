0e1dde5a6670d679166a23277faef549d7911501
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-23 23:31:42 -0700

    LC-find-words-containing-character [Runtime: 1 ms; Memory: 44.9 MB]

--- array/find-words-containing-character.java
+++ array/find-words-containing-character.java
@@ -2,7 +2,6 @@ import java.util.*;
 class Solution {
     /**
      * This method returns a list of indices of words that contain the given character x.
-     * @param words An array of lowercase strings
      */
     public List<Integer> findWordsContaining(String[] words, char x) {
         List<Integer> result = new ArrayList<>();
