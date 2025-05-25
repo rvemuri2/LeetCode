b2238a7fcd2860bed26aaa19f3374ec201b17fe8
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-24 17:00:50 -0700

    LC-house-robber [Runtime: 0 ms; Memory: 41.6 MB]

--- dynamic-programming/house-robber.java
+++ dynamic-programming/house-robber.java
@@ -28,8 +28,7 @@ class Solution {
     // Example test
     public static void main(String[] args) {
         Solution sol = new Solution();
-
-        System.out.println(sol.rob(new int[]{1, 2, 3, 1})); // Output: 4
+        System.out.println(sol.rob(new int[]{1, 2, 3, 1}));
         System.out.println(sol.rob(new int[]{2, 7, 9, 3, 1}));
     }
 }
