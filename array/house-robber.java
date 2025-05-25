109dd1840c1e87e10393966ab5a770e833b80936
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-24 17:02:58 -0700

    LC-house-robber [Runtime: 0 ms; Memory: 41.5 MB]

--- array/house-robber.java
+++ array/house-robber.java
@@ -13,7 +13,7 @@ class Solution {
         
         // Base cases
         dp[0] = nums[0];                        // Rob first house
-        dp[1] = Math.max(nums[0], nums[1]);     // Rob richer of first two
+        dp[1] = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < n; i++) {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
