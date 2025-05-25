95fa2bdad09e7a88eb36c498943d50dc6bbaa9b2
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-24 17:00:39 -0700

    LC-house-robber [Runtime: 0 ms; Memory: 41.5 MB]

--- array/house-robber.java
+++ array/house-robber.java
@@ -30,6 +30,6 @@ class Solution {
         Solution sol = new Solution();
 
         System.out.println(sol.rob(new int[]{1, 2, 3, 1})); // Output: 4
-        System.out.println(sol.rob(new int[]{2, 7, 9, 3, 1})); // Output: 12
+        System.out.println(sol.rob(new int[]{2, 7, 9, 3, 1}));
     }
 }
