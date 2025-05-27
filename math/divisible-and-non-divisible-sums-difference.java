b27bc15fd922aa1672c8ce91ff3eae60d477dab4
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-26 22:16:19 -0700

    LC-divisible-and-non-divisible-sums-difference [Runtime: 0 ms; Memory: 41.1 MB]

--- math/divisible-and-non-divisible-sums-difference.java
+++ math/divisible-and-non-divisible-sums-difference.java
@@ -16,6 +16,5 @@ class Solution {
     // Test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.differenceOfSums(10, 3));
     }
 }
