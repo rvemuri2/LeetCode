5684ef5e54fab4820f17e22402c10f3d484d27e9
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-26 22:17:13 -0700

    LC-divisible-and-non-divisible-sums-difference [Runtime: 0 ms; Memory: 40.6 MB]

--- math/divisible-and-non-divisible-sums-difference.java
+++ math/divisible-and-non-divisible-sums-difference.java
@@ -2,8 +2,6 @@ class Solution {
     public int differenceOfSums(int n, int m) {
         // Sum of all integers from 1 to n
         long totalSum = (long) n * (n + 1) / 2;
-
-        // Count of multiples of m in [1, n]
         long countDiv = n / m;
         long divisibleSum = m * countDiv * (countDiv + 1) / 2;
         return (int)(totalSum - 2 * divisibleSum);
