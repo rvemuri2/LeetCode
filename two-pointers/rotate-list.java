7902f60d1340a55e3f5209292d5722df3c9c5c42
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-27 22:53:04 -0700

    LC-rotate-list [Runtime: 0 ms; Memory: 42.2 MB]

---
+++ two-pointers/rotate-list.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        // Step 1: Find the length of the list
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        // Step 2: Compute effective rotation
+        k = k % length;
+        if (k == 0) return head;
+
+        // Step 3: Make it circular temporarily
+        tail.next = head;
+
+        // Step 4: Find the new tail: length - k - 1 steps from head
+        ListNode newTail = head;
+        for (int i = 0; i < length - k - 1; i++) {
+            newTail = newTail.next;
+        }
+
+        // Step 5: New head is next of newTail, break the link
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
