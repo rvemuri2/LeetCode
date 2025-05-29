6dc0a4324f38c2eddcc607f6401f09deb4974167
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-28 23:35:29 -0700

    LC-maximize-the-number-of-target-nodes-after-connecting-trees-ii [Runtime: 132 ms; Memory: 107.6 MB]

--- tree/maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
+++ tree/maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
@@ -76,6 +76,5 @@ public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[][] edges1 = {{0,1},{0,2},{2,3},{2,4}};
-        int[][] edges2 = {{0,1},{0,2},{0,3},{2,7},{1,4},{4,5},{4,6}};
     }
 }
