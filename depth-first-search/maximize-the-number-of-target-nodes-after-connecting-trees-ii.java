46c5d8229a40596608ec8da2792a43d607e7ef9a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-05-28 23:35:21 -0700

    LC-maximize-the-number-of-target-nodes-after-connecting-trees-ii [Runtime: 129 ms; Memory: 125.5 MB]

--- depth-first-search/maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
+++ depth-first-search/maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
@@ -77,6 +77,5 @@ public class Solution {
         Solution sol = new Solution();
         int[][] edges1 = {{0,1},{0,2},{2,3},{2,4}};
         int[][] edges2 = {{0,1},{0,2},{0,3},{2,7},{1,4},{4,5},{4,6}};
-        System.out.println(Arrays.toString(sol.maxTargetNodes(edges1, edges2)));
     }
 }
