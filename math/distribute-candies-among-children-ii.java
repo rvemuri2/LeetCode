296c32f51c217274192edb95c7c87b345ccab8c8
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-01 23:01:15 -0700

    LC-distribute-candies-among-children-ii [Runtime: 1 ms; Memory: 40.8 MB]

--- math/distribute-candies-among-children-ii.java
+++ math/distribute-candies-among-children-ii.java
@@ -29,6 +29,6 @@ class Solution {
     // Optional main for quick testing
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.distributeCandies(5, 2)); // 3
+        System.out.println(sol.distributeCandies(5, 2));
     }
 }
