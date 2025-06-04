b16c7b58ed2fab4ae675f5b229c53cac2bb4a0ed
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-03 22:17:34 -0700

    LC-find-the-lexicographically-largest-string-from-the-box-i [Runtime: 73 ms; Memory: 43.4 MB]

--- enumeration/find-the-lexicographically-largest-string-from-the-box-i.java
+++ enumeration/find-the-lexicographically-largest-string-from-the-box-i.java
@@ -54,6 +54,6 @@ class Solution {
         System.out.println(sol.answerString("dbca", 2)); // "dbc"
         System.out.println(sol.answerString("gggg", 4)); // "g"
         // Special case k=1:
-        System.out.println(sol.answerString("gh", 1));   // "gh"
+        System.out.println(sol.answerString("gh", 1));
     }
 }
