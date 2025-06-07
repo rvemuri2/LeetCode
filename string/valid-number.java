34c90a0236ccd56be26a1fa418ac0f66aca3bb8a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-06 22:31:59 -0700

    LC-valid-number [Runtime: 1 ms; Memory: 42.5 MB]

--- string/valid-number.java
+++ string/valid-number.java
@@ -81,6 +81,5 @@ class Solution {
         // Edge cases
         System.out.println(sol.isNumber("."));         // false
         System.out.println(sol.isNumber(".1"));        // true
-        System.out.println(sol.isNumber("3."));
     }
 }
