5fb7367d51c84a07e1923a49a07eb771f72ed3d2
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-06 22:34:35 -0700

    LC-valid-number [Runtime: 1 ms; Memory: 42.5 MB]

--- string/valid-number.java
+++ string/valid-number.java
@@ -71,6 +71,6 @@ class Solution {
         // Invalid numbers
         System.out.println(sol.isNumber("abc"));       // false
         System.out.println(sol.isNumber("1a"));        // false
-        System.out.println(sol.isNumber("1e"));        // false
+        System.out.println(sol.isNumber("1e"));
     }
 }
