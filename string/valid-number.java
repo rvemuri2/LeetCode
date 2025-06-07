4e0c734672fe5bbcbf4ac1354194460ebc5a1949
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-06 22:35:53 -0700

    LC-valid-number [Runtime: 1 ms; Memory: 42.9 MB]

--- string/valid-number.java
+++ string/valid-number.java
@@ -65,6 +65,6 @@ class Solution {
         System.out.println(sol.isNumber("-90E3"));     // true
         System.out.println(sol.isNumber("3e+7"));      // true
         System.out.println(sol.isNumber("+6e-1"));     // true
-        System.out.println(sol.isNumber("53.5e93"));   // true
+        System.out.println(sol.isNumber("53.5e93"));
     }
 }
