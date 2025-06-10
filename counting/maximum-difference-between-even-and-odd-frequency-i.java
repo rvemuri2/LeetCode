16485adcac1b52f95d34fc386a7c2f40c09eeabd
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-09 22:05:03 -0700

    LC-maximum-difference-between-even-and-odd-frequency-i [Runtime: 1 ms; Memory: 42.3 MB]

--- counting/maximum-difference-between-even-and-odd-frequency-i.java
+++ counting/maximum-difference-between-even-and-odd-frequency-i.java
@@ -38,6 +38,6 @@ class Solution {
     // Optional main for testing
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.maxDifference("aaaaabbc"));  // 3
+        System.out.println(sol.maxDifference("aaaaabbc"));
     }
 }
