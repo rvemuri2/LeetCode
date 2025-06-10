8382fdd27df570a71c31c9e48d415dca6ce46e4a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-09 22:05:59 -0700

    LC-maximum-difference-between-even-and-odd-frequency-i [Runtime: 1 ms; Memory: 42.4 MB]

--- hash-table/maximum-difference-between-even-and-odd-frequency-i.java
+++ hash-table/maximum-difference-between-even-and-odd-frequency-i.java
@@ -22,7 +22,7 @@ class Solution {
         // - max frequency among characters with odd count
         // - min frequency among characters with even count
         for (int f : freq) {
-            if (f == 0) continue; // skip unused letters
+            if (f == 0) continue;
 
             if (f % 2 == 1) {
                 maxOdd = Math.max(maxOdd, f);
