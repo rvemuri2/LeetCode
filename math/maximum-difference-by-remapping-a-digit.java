ca3994f5c350e8f12e270029b2bc077f69c1b131
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-13 23:45:19 -0700

    LC-maximum-difference-by-remapping-a-digit [Runtime: 1 ms; Memory: 40.7 MB]

--- math/maximum-difference-by-remapping-a-digit.java
+++ math/maximum-difference-by-remapping-a-digit.java
@@ -35,6 +35,5 @@ class Solution {
         Solution sol = new Solution();
         System.out.println("Test 1: " + sol.minMaxDifference(11891)); // Expected: 99009
         System.out.println("Test 2: " + sol.minMaxDifference(90));    // Expected: 99
-        System.out.println("Test 3: " + sol.minMaxDifference(99999));
     }
 }
