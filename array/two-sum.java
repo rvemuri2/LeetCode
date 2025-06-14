05c9a958fda860a06a87d778bcba25dde54f7e3b
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-13 23:49:07 -0700

    LC-two-sum [Runtime: 2 ms; Memory: 45.2 MB]

--- array/two-sum.java
+++ array/two-sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target) {
-                    return new int[]{i,j};
-                }
-                
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
             }
+            numMap.put(nums[i], i);
         }
-        return new int []{};
-        
+        return new int[]{}; // Fallback (problem guarantees a solution)
     }
 }
