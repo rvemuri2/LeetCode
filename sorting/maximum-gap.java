b353b51428e37fc27c6f23ec90185d690d5f4872
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-16 16:35:24 -0700

    LC-maximum-gap [Runtime: 12 ms; Memory: 62.3 MB]

--- sorting/maximum-gap.java
+++ sorting/maximum-gap.java
@@ -50,6 +50,5 @@ class Solution {
     // Test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.maximumGap(new int[]{3, 6, 9, 1}));
     }
 }
