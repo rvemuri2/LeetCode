5ce87caf38bcd7a2e4cba9faf14aa395c40eadca
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-16 15:06:02 -0700

    LC-reverse-bits [Runtime: 0 ms; Memory: 41.9 MB]

--- divide-and-conquer/reverse-bits.java
+++ divide-and-conquer/reverse-bits.java
@@ -3,7 +3,6 @@ public class Solution {
     public int reverseBits(int n) {
         int result = 0;
 
-        // Loop through 32 bits of the integer
         for (int i = 0; i < 32; i++) {
 
             result <<= 1;
