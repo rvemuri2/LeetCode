f7a130e0ca2ff3729e522e4f3c74bde770b6f907
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-15 23:49:20 -0700

    LC-3sum-closest [Runtime: 13 ms; Memory: 43.7 MB]

--- two-pointers/3sum-closest.java
+++ two-pointers/3sum-closest.java
@@ -9,7 +9,6 @@ class Solution {
         // Initialize result with a very large value
         int closestSum = nums[0] + nums[1] + nums[2];
 
-        // Iterate through the array
         for (int i = 0; i < n - 2; i++) {
             int left = i + 1;
             int right = n - 1;
