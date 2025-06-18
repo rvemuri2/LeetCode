db7aa2b0ec84f1406eb81dfb13e68773e42bcc41
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-18 16:12:57 -0700

    LC-divide-array-into-arrays-with-max-difference [Runtime: 19 ms; Memory: 61.2 MB]

--- greedy/divide-array-into-arrays-with-max-difference.java
+++ greedy/divide-array-into-arrays-with-max-difference.java
@@ -35,6 +35,6 @@ class Solution {
 
         int[] nums2 = {2,4,2,2,5,2};
         int k2 = 2;
-        System.out.println(Arrays.deepToString(sol.divideArray(nums2, k2))); // Expected: []
+        System.out.println(Arrays.deepToString(sol.divideArray(nums2, k2)));
     }
 }
