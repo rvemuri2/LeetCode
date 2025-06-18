efb766510da128d49b787b3ca7b4afd38618d8b6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-17 23:59:05 -0700

    LC-find-peak-element [Runtime: 0 ms; Memory: 42.5 MB]

--- binary-search/find-peak-element.java
+++ binary-search/find-peak-element.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
+        int l = 0;
         int right = nums.length - 1;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        while (l < right) {
+            int mid = l + (right - l) / 2;
             if (nums[mid] < nums[mid + 1]) {
-                left = mid + 1;
+                l = mid + 1;
             } else {
                 right = mid;
             }
         }
-        return left;
+        return l;
     }
 }
