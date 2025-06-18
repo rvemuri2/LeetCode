abd96de1ed2364cd18ae2e1ab6eba8823ff5000d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-18 16:14:30 -0700

    LC-divide-array-into-arrays-with-max-difference [Runtime: 21 ms; Memory: 61.2 MB]

--- greedy/divide-array-into-arrays-with-max-difference.java
+++ greedy/divide-array-into-arrays-with-max-difference.java
@@ -32,7 +32,5 @@ class Solution {
         int[] nums1 = {1,3,4,8,7,9,3,5,1};
         int k1 = 2;
         System.out.println(Arrays.deepToString(sol.divideArray(nums1, k1))); // Expected: Valid triplets
-
-        int[] nums2 = {2,4,2,2,5,2};
     }
 }
