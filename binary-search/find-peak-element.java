f9916dcf0db86080da1ab6a1033cda8f9ca6e2ab
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-17 23:56:46 -0700

    LC-find-peak-element [Runtime: 0 ms; Memory: 42.5 MB]

--- binary-search/find-peak-element.java
+++ binary-search/find-peak-element.java
@@ -23,6 +23,5 @@ class Solution {
     // Test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.findPeakElement(new int[]{1, 2, 3, 1}));
     }
 }
