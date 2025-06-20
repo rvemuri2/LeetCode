a6060fddeb7d741c7d04b5d50d742a2f45c44ce0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-19 23:32:57 -0700

    LC-generate-binary-strings-without-adjacent-zeros [Runtime: 3 ms; Memory: 45.7 MB]

---
+++ bit-manipulation/generate-binary-strings-without-adjacent-zeros.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<String> validStrings(int n) {
+        List<String> result = new ArrayList<>();
+        dfs(n, "", result);
+        return result;
+    }
+
+    private void dfs(int n, String current, List<String> result) {
+        if (current.length() == n) {
+            result.add(current);
+            return;
+        }
+
+        // If empty or last char is '1', we can add both '0' and '1'
+        if (current.isEmpty() || current.charAt(current.length() - 1) == '1') {
+            dfs(n, current + "0", result);
+        }
+
+        // Always allowed to add '1'
+        dfs(n, current + "1", result);
+    }
+}
