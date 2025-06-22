b0cf9fde7353d2ff176679fd9b99368cb86acdff
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-21 23:04:41 -0700

    LC-divide-a-string-into-groups-of-size-k [Runtime: 1 ms; Memory: 42.2 MB]

--- simulation/divide-a-string-into-groups-of-size-k.java
+++ simulation/divide-a-string-into-groups-of-size-k.java
@@ -30,6 +30,5 @@ class Solution {
     // Example test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(Arrays.toString(sol.divideString("abcdefghi", 3, 'x')));
     }
 }
