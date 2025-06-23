03a0d9df788ad9eecbd125ee51053d1ce25ad63d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-22 21:36:18 -0700

    LC-longest-valid-parentheses [Runtime: 3 ms; Memory: 43.4 MB]

--- dynamic-programming/longest-valid-parentheses.java
+++ dynamic-programming/longest-valid-parentheses.java
@@ -35,6 +35,6 @@ class Solution {
         System.out.println(sol.longestValidParentheses("(()"));       // Output: 2
         System.out.println(sol.longestValidParentheses(")()())"));    // Output: 4
         System.out.println(sol.longestValidParentheses(""));          // Output: 0
-        System.out.println(sol.longestValidParentheses("()(()"));     // Output: 2
+        System.out.println(sol.longestValidParentheses("()(()"));
     }
 }
