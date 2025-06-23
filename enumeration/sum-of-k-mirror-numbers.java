8a629eefb1c7e79c24d3e65d4157226c2b886e02
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-22 21:21:59 -0700

    LC-sum-of-k-mirror-numbers [Runtime: 1026 ms; Memory: 44.6 MB]

--- enumeration/sum-of-k-mirror-numbers.java
+++ enumeration/sum-of-k-mirror-numbers.java
@@ -46,6 +46,5 @@ class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.kMirror(2, 5)); // Output: 25
-        System.out.println(sol.kMirror(3, 7));
     }
 }
