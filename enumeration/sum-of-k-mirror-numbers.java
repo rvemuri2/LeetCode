4a1b9458a82e5e4ce7468497e465af9eefcc49eb
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-22 21:24:53 -0700

    LC-sum-of-k-mirror-numbers [Runtime: 1086 ms; Memory: 44.9 MB]

--- enumeration/sum-of-k-mirror-numbers.java
+++ enumeration/sum-of-k-mirror-numbers.java
@@ -3,7 +3,9 @@ class Solution {
     private boolean isPalindrome(String s) {
         int l = 0, r = s.length() - 1;
         while (l < r) {
-            if (s.charAt(l++) != s.charAt(r--)) return false;
+            if (s.charAt(l++) != s.charAt(r--)) {
+                return false;
+            }
         }
         return true;
     }
