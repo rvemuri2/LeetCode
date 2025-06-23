c8de74015afddd32241b808565ccb85bbdf3b724
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-23 10:56:27 -0700

    LC-longest-valid-parentheses [Runtime: 4 ms; Memory: 43.5 MB]

--- stack/longest-valid-parentheses.java
+++ stack/longest-valid-parentheses.java
@@ -2,13 +2,10 @@ class Solution {
     public int longestValidParentheses(String s) {
         // Stack to store indices
         Stack<Integer> stack = new Stack<>();
-        // Initialize with -1 to handle base case for first valid substring
         stack.push(-1);
         int maxLength = 0;
-
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-
             if (c == '(') {
                 stack.push(i);
             } else {
