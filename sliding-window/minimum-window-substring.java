aa6f8116168ff5dcd26711cd881082059ddc88b3
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-25 23:31:54 -0700

    LC-minimum-window-substring [Runtime: 19 ms; Memory: 45.5 MB]

--- sliding-window/minimum-window-substring.java
+++ sliding-window/minimum-window-substring.java
@@ -54,6 +54,5 @@ class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.minWindow("ADOBECODEBANC", "ABC")); // "BANC"
-        System.out.println(sol.minWindow("a", "a"));
     }
 }
