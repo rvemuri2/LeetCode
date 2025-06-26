b620f1317ab0048cfb7a566c6a5d0daf8bb18441
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-25 23:33:26 -0700

    LC-minimum-window-substring [Runtime: 19 ms; Memory: 45.3 MB]

--- sliding-window/minimum-window-substring.java
+++ sliding-window/minimum-window-substring.java
@@ -50,5 +50,4 @@ class Solution {
         return res[0] == -1 ? "" : s.substring(res[1], res[2] + 1);
     }
     // Optional: main method for testing
-    public static void main(String[] args) {}
 }
