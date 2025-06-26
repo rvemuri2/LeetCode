adebd058a0992f8e7947a8b5273c6047b09bedf4
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-25 23:37:01 -0700

    LC-summary-ranges [Runtime: 5 ms; Memory: 41.9 MB]

--- array/summary-ranges.java
+++ array/summary-ranges.java
@@ -32,6 +32,5 @@ class Solution {
         System.out.println(sol.summaryRanges(new int[]{0, 1, 2, 4, 5, 7}));
         System.out.println(sol.summaryRanges(new int[]{0, 2, 3, 4, 6, 8, 9}));
         System.out.println(sol.summaryRanges(new int[]{}));
-        System.out.println(sol.summaryRanges(new int[]{1}));
     }
 }
