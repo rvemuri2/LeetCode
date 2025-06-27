c44b9406f47d0f8e26eabddf607cfb9884391212
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-26 23:27:45 -0700

    LC-implement-queue-using-stacks [Runtime: 0 ms; Memory: 41.5 MB]

---
+++ stack/implement-queue-using-stacks.java
@@ -0,0 +1,63 @@
+import java.util.Stack;
+
+class MyQueue {
+    Stack<Integer> stack1; // to handle push operations
+    Stack<Integer> stack2; // to handle pop and peek operations
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.isEmpty()) {
+            // Transfer elements from stack1 to stack2 to reverse order
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (stack2.isEmpty()) {
+            // Transfer elements from stack1 to stack2 to reverse order
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    /** Optional: Quick test example */
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue: [1]
+        myQueue.push(2); // queue: [1, 2]
+        System.out.println(myQueue.peek()); // return 1
+        System.out.println(myQueue.pop());  // return 1
+        System.out.println(myQueue.empty()); // return false
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
