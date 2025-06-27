7941dbb86da77a3704ecb2175e95fd92396b4275
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-26 23:29:16 -0700

    LC-implement-queue-using-stacks [Runtime: 0 ms; Memory: 41.3 MB]

--- queue/implement-queue-using-stacks.java
+++ queue/implement-queue-using-stacks.java
@@ -46,7 +46,7 @@ class MyQueue {
     public static void main(String[] args) {
         MyQueue myQueue = new MyQueue();
         myQueue.push(1); // queue: [1]
-        myQueue.push(2); // queue: [1, 2]
+        myQueue.push(2);
     }
 }
 
