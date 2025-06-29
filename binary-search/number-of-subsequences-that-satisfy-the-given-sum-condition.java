7ef750590f1485126b50be85cb189f7b3451fde8
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-28 23:45:58 -0700

    LC-number-of-subsequences-that-satisfy-the-given-sum-condition [Runtime: 27 ms; Memory: 55.4 MB]

---
+++ binary-search/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int MOD = 1_000_000_007;
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        // Precompute powers of 2 up to n
+        int[] pow2 = new int[n];
+        pow2[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int left = 0, right = n - 1;
+        int res = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                // Number of subsequences is 2^(right-left)
+                res = (res + pow2[right - left]) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return res;
+    }
+
+    /** Optional: Quick test */
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.numSubseq(new int[]{3,5,6,7}, 9)); // 4
+        System.out.println(sol.numSubseq(new int[]{3,3,6,8}, 10)); // 6
+        System.out.println(sol.numSubseq(new int[]{2,3,3,4,6,7}, 12)); // 61
+    }
+}
