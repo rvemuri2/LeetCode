edc9b5b0a1a977c04ed0d8645b6bbb6010ab6d2e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-28 23:46:14 -0700

    LC-number-of-subsequences-that-satisfy-the-given-sum-condition [Runtime: 27 ms; Memory: 55.6 MB]

--- two-pointers/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ two-pointers/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -34,6 +34,6 @@ class Solution {
         Solution sol = new Solution();
         System.out.println(sol.numSubseq(new int[]{3,5,6,7}, 9)); // 4
         System.out.println(sol.numSubseq(new int[]{3,3,6,8}, 10)); // 6
-        System.out.println(sol.numSubseq(new int[]{2,3,3,4,6,7}, 12)); // 61
+        System.out.println(sol.numSubseq(new int[]{2,3,3,4,6,7}, 12));
     }
 }
