310d057714e0d010f18b24eeeb666af621544f08
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-29 22:24:45 -0700

    LC-longest-harmonious-subsequence [Runtime: 16 ms; Memory: 46 MB]

--- sorting/longest-harmonious-subsequence.java
+++ sorting/longest-harmonious-subsequence.java
@@ -4,12 +4,11 @@ class Solution {
     public int findLHS(int[] nums) {
         HashMap<Integer, Integer> freq = new HashMap<>();
         int maxLen = 0;
-
-        // Count frequencies of each number
+        
         for (int num : nums) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-        
+
         for (int key : freq.keySet()) {
             if (freq.containsKey(key + 1)) {
                 maxLen = Math.max(maxLen, freq.get(key) + freq.get(key + 1));
