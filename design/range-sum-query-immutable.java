0a18c998786359b289866559d5e0b01cd720ff33
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-06-30 23:36:54 -0700

    LC-range-sum-query-immutable [Runtime: 7 ms; Memory: 49.4 MB]

--- design/range-sum-query-immutable.java
+++ design/range-sum-query-immutable.java
@@ -19,7 +19,6 @@ class NumArray {
 
     public static void main(String[] args) {
         NumArray numArray = new NumArray(new int[] {-2, 0, 3, -5, 2, -1});
-        System.out.println(numArray.sumRange(0, 2));
     }
 }
 
