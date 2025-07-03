beaa348e63a1801e463a8bf4afb7eeecd818c6b9
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-02 22:49:34 -0700

    LC-missing-number [Runtime: 0 ms; Memory: 45.4 MB]

---
+++ bit-manipulation/missing-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        // Calculate expected sum from 0 to n
+        int expectedSum = n * (n + 1) / 2;
+
+        // Calculate actual sum of nums
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        // The missing number is the difference
+        return expectedSum - actualSum;
+    }
+
+    // Test examples
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {3,0,1};
+        System.out.println(sol.missingNumber(nums1)); // Output: 2
+
+        int[] nums2 = {0,1};
+        System.out.println(sol.missingNumber(nums2)); // Output: 2
+
+        int[] nums3 = {9,6,4,2,3,5,7,0,1};
+        System.out.println(sol.missingNumber(nums3)); // Output: 8
+    }
+}
