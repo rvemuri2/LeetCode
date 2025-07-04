c141ee15729f7411860c24dc3e77518199562119
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-03 23:55:52 -0700

    LC-find-the-k-th-character-in-string-game-ii [Runtime: 1 ms; Memory: 43.3 MB]

--- recursion/find-the-k-th-character-in-string-game-ii.java
+++ recursion/find-the-k-th-character-in-string-game-ii.java
@@ -5,10 +5,9 @@ class Solution {
         long[] len = new long[m + 1];
         len[0] = 1;
         for (int i = 0; i < m; i++) {
-            // both op 0 and op 1 double the length
             len[i + 1] = Math.min(len[i] * 2, k);
         }
-        
+
         int shiftCount = 0;
         for (int i = m - 1; i >= 0; i--) {
             long half = len[i];
