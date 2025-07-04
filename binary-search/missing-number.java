78504ec51866f5e2022896927139f96386a1cc52
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-03 23:48:38 -0700

    LC-missing-number [Runtime: 0 ms; Memory: 45.5 MB]

--- binary-search/missing-number.java
+++ binary-search/missing-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-
-        // Calculate expected sum from 0 to n
-        int expectedSum = n * (n + 1) / 2;
         
+        int expectedSum = n * (n + 1) / 2;
+
         int actualSum = 0;
         for (int num : nums) {
             actualSum += num;
