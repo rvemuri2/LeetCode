c685364feba758a6c56a9503afd1c07f7fffec3d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-03 23:48:06 -0700

    LC-missing-number [Runtime: 0 ms; Memory: 44.8 MB]

--- array/missing-number.java
+++ array/missing-number.java
@@ -4,8 +4,7 @@ class Solution {
 
         // Calculate expected sum from 0 to n
         int expectedSum = n * (n + 1) / 2;
-
-        // Calculate actual sum of nums
+        
         int actualSum = 0;
         for (int num : nums) {
             actualSum += num;
