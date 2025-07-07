36ff34c13c72d7d07feacb2f57248165f1d21a49
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-06 23:15:29 -0700

    LC-finding-pairs-with-a-certain-sum [Runtime: 140 ms; Memory: 73.2 MB]

--- array/finding-pairs-with-a-certain-sum.java
+++ array/finding-pairs-with-a-certain-sum.java
@@ -56,5 +56,4 @@ class FindSumPairs {
  * Your FindSumPairs object will be instantiated and called as such:
  * FindSumPairs obj = new FindSumPairs(nums1, nums2);
  * obj.add(index,val);
- * int param_2 = obj.count(tot);
  */
