a97e487520a4e9dcbcb5585b4ee89c889275a7b6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-06 23:31:47 -0700

    LC-finding-pairs-with-a-certain-sum [Runtime: 136 ms; Memory: 73.1 MB]

--- design/finding-pairs-with-a-certain-sum.java
+++ design/finding-pairs-with-a-certain-sum.java
@@ -10,7 +10,7 @@ class FindSumPairs {
             freq2.put(x, freq2.getOrDefault(x, 0) + 1);
         }
     }
-    //Adding method
+    //Adding
     public void add(int index, int val) {
         int old = nums2[index];
         int updated = old + val;
