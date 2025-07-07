8e2c656db9cf7c92957dee8558cd513212606185
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-06 23:15:40 -0700

    LC-finding-pairs-with-a-certain-sum [Runtime: 142 ms; Memory: 73.6 MB]

--- design/finding-pairs-with-a-certain-sum.java
+++ design/finding-pairs-with-a-certain-sum.java
@@ -48,8 +48,7 @@ class FindSumPairs {
     }
 
     // ---------- Simple test -------------
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) {}
 }
 
 /**
