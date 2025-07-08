d1777e0676f528c7ab278393b9dc42fd85c642cb
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-07 23:37:15 -0700

    LC-find-the-town-judge [Runtime: 3 ms; Memory: 54.1 MB]

--- array/find-the-town-judge.java
+++ array/find-the-town-judge.java
@@ -27,6 +27,5 @@ class Solution {
     // Simple tests
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.findJudge(2, new int[][]{{1,2}}));
     }
 }
