cf82243e7597a5b978fa847be2bcfaf074784cad
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-08 23:57:36 -0700

    LC-isomorphic-strings [Runtime: 17 ms; Memory: 42.7 MB]

--- hash-table/isomorphic-strings.java
+++ hash-table/isomorphic-strings.java
@@ -36,6 +36,5 @@ class Solution {
 
         System.out.println(sol.isIsomorphic("egg", "add")); // true
         System.out.println(sol.isIsomorphic("foo", "bar")); // false
-        System.out.println(sol.isIsomorphic("paper", "title"));
     }
 }
