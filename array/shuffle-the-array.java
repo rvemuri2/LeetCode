e00911fa7829d677f4cbc0fae5a1124eda16ba93
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-10 15:02:14 -0700

    LC-shuffle-the-array [Runtime: 0 ms; Memory: 44.8 MB]

--- array/shuffle-the-array.java
+++ array/shuffle-the-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        // Initialize result array of size 2n
         int[] result = new int[2 * n];
         int j = n;
         for (int i = 0; i < n; i++) {
