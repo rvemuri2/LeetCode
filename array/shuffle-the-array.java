760d1c383a953181e002b69320ff352452ee2f1c
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-10 15:01:51 -0700

    LC-shuffle-the-array [Runtime: 0 ms; Memory: 45.1 MB]

--- array/shuffle-the-array.java
+++ array/shuffle-the-array.java
@@ -6,7 +6,6 @@ class Solution {
         // Use two pointers: i for x's (0..n-1) and j for y's (n..2n-1)
         int j = n;
         for (int i = 0; i < n; i++) {
-            // Place xi at even indices and yi at odd indices
             result[2 * i] = nums[i];
             result[2 * i + 1] = nums[j];
             j++;
