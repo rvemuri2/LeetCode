e63ffd87fb554e4355a0ebba2b98046f7bca5f65
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-10 15:02:02 -0700

    LC-shuffle-the-array [Runtime: 0 ms; Memory: 44.6 MB]

--- array/shuffle-the-array.java
+++ array/shuffle-the-array.java
@@ -2,8 +2,6 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         // Initialize result array of size 2n
         int[] result = new int[2 * n];
-
-        // Use two pointers: i for x's (0..n-1) and j for y's (n..2n-1)
         int j = n;
         for (int i = 0; i < n; i++) {
             result[2 * i] = nums[i];
