dcac857425e1202a7223ddcf28ba810e23022612
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-09 23:39:36 -0700

    LC-convert-the-temperature [Runtime: 0 ms; Memory: 41.8 MB]

--- math/convert-the-temperature.java
+++ math/convert-the-temperature.java
@@ -3,7 +3,6 @@ class Solution {
         // Formula for Kelvin: Celsius + 273.15
         double kelvin = celsius + 273.15;
 
-        // Formula for Fahrenheit: Celsius * 1.80 + 32.00
         double fahrenheit = celsius * 1.80 + 32.00;
 
         return new double[]{kelvin, fahrenheit};
