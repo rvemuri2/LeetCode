12477e43c444ccf0cd7e12deb0b69ff142f05db2
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-11 23:42:13 -0700

    LC-contains-duplicate-ii [Runtime: 22 ms; Memory: 60.2 MB]

--- array/contains-duplicate-ii.java
+++ array/contains-duplicate-ii.java
@@ -20,6 +20,5 @@ class Solution {
     // Example test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
     }
 }
