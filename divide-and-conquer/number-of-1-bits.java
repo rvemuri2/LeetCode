5cf7b207af33d8ab6c9bef6201e361e6decc08f1
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-11 23:36:15 -0700

    LC-number-of-1-bits [Runtime: 0 ms; Memory: 40.6 MB]

---
+++ divide-and-conquer/number-of-1-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);  // drops the lowest set bit
+            count++;
+        }
+        return count;
+    }
+
+    // Test cases
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.hammingWeight(11));         // Output: 3 (binary: 1011)
+        System.out.println(sol.hammingWeight(128));        // Output: 1 (binary: 10000000)
+        System.out.println(sol.hammingWeight(2147483645)); // Output: 30 (binary: 111...1101)
+        System.out.println(sol.hammingWeight(0));          // Output: 0
+    }
+}
