76b6fb23443955b0e11e0466636486d1b9ab9a14
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-11 23:40:20 -0700

    LC-contains-duplicate-ii [Runtime: 23 ms; Memory: 59.5 MB]

--- sliding-window/contains-duplicate-ii.java
+++ sliding-window/contains-duplicate-ii.java
@@ -23,6 +23,5 @@ class Solution {
         System.out.println(sol.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));    // true
         System.out.println(sol.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));    // true
         System.out.println(sol.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2)); // false
-        System.out.println(sol.containsNearbyDuplicate(new int[]{99, 99}, 2));
     }
 }
