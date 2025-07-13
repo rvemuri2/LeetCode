12e50fadf9c35fdce95fb7fe86ee548f760e78db
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-12 23:43:03 -0700

    LC-nim-game [Runtime: 0 ms; Memory: 41 MB]

---
+++ game-theory/nim-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        // You lose only when n is divisible by 4
+        return n % 4 != 0;
+    }
+
+    // Example test cases
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.canWinNim(4));  // false
+        System.out.println(sol.canWinNim(1));  // true
+        System.out.println(sol.canWinNim(2));  // true
+        System.out.println(sol.canWinNim(7));  // true
+        System.out.println(sol.canWinNim(8));  // false
+    }
+}
