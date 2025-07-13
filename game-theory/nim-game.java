c3ba858fd2e7b419ad0f0ab66712971e3ffde3f8
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-12 23:43:42 -0700

    LC-nim-game [Runtime: 0 ms; Memory: 41.2 MB]

--- game-theory/nim-game.java
+++ game-theory/nim-game.java
@@ -10,6 +10,6 @@ class Solution {
         System.out.println(sol.canWinNim(4));  // false
         System.out.println(sol.canWinNim(1));  // true
         System.out.println(sol.canWinNim(2));  // true
-        System.out.println(sol.canWinNim(7));  // true
+        System.out.println(sol.canWinNim(7));
     }
 }
