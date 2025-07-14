1083f8bfafa9f46e0fedb044b8f8072e76a5b014
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-13 21:08:07 -0700

    LC-find-the-difference [Runtime: 1 ms; Memory: 42.2 MB]

---
+++ sorting/find-the-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char res = 0;
+        for (char c : s.toCharArray()) {
+            res ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            res ^= c;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.findTheDifference("abcd", "abcde")); // Output: e
+        System.out.println(sol.findTheDifference("", "y"));         // Output: y
+        System.out.println(sol.findTheDifference("a", "aa"));       // Output: a
+        System.out.println(sol.findTheDifference("ae", "aea"));     // Output: a
+        System.out.println(sol.findTheDifference("xyz", "xxyz"));   // Output: x
+    }
+}
