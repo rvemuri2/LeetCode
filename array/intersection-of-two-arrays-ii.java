3b71546725aeed3389ab95c8be82e31bf939c170
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-13 21:06:36 -0700

    LC-intersection-of-two-arrays-ii [Runtime: 3 ms; Memory: 43.9 MB]

--- array/intersection-of-two-arrays-ii.java
+++ array/intersection-of-two-arrays-ii.java
@@ -10,14 +10,13 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Check matches with nums2
         for (int num : nums2) {
             if (map.containsKey(num) && map.get(num) > 0) {
                 result.add(num);
                 map.put(num, map.get(num) - 1);
             }
         }
-        
+
         int[] intersection = new int[result.size()];
         for (int i = 0; i < result.size(); i++) {
             intersection[i] = result.get(i);
