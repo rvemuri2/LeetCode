b3325818794334a98f31ce64e2e87b2e74760e54
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-15 17:37:14 -0700

    LC-maximum-matching-of-players-with-trainers [Runtime: 26 ms; Memory: 57.6 MB]

--- sorting/maximum-matching-of-players-with-trainers.java
+++ sorting/maximum-matching-of-players-with-trainers.java
@@ -1,23 +1,23 @@
-public class Solution {
+class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-
         Arrays.sort(players);
         Arrays.sort(trainers);
         
-        int i = 0;
-        int j = 0;
         int count = 0;
+        int i = 0; // pointer for players
+        int j = 0; // pointer for trainers
         
         while (i < players.length && j < trainers.length) {
             if (players[i] <= trainers[j]) {
+                // Match found, move both pointers
                 count++;
                 i++;
                 j++;
             } else {
+                // Current trainer can't train this player, try next trainer
                 j++;
             }
         }
-        
         return count;
     }
 }
