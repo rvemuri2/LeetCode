c2865028b26f8abdae4a9e5550475f9f416d1b93
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-15 17:35:09 -0700

    LC-maximum-matching-of-players-with-trainers [Runtime: 26 ms; Memory: 65.6 MB]

--- sorting/maximum-matching-of-players-with-trainers.java
+++ sorting/maximum-matching-of-players-with-trainers.java
@@ -19,7 +19,6 @@ public class Solution {
                 i++;
                 j++;
             } else {
-                // Trainer[j] is too weak, try next stronger trainer
                 j++;
             }
         }
