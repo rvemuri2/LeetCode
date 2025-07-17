3cf6525f7dd90869af23e960d431216eef61a78a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-16 23:34:22 -0700

    LC-find-the-maximum-length-of-valid-subsequence-i [Runtime: 8 ms; Memory: 62.2 MB]

--- array/find-the-maximum-length-of-valid-subsequence-i.java
+++ array/find-the-maximum-length-of-valid-subsequence-i.java
@@ -96,6 +96,6 @@ class Solution {
         System.out.println("Test Case 2 (ladderLength): " + sol.ladderLength("hit", "cog", wordList2)); // Output: 0
 
         // Test maximumLength
-        int[] nums1 = {1, 2, 3, 4, 5}; // alternating best: 5
+        int[] nums1 = {1, 2, 3, 4, 5};
     }
 }
