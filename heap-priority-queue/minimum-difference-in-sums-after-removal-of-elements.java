c51cf1b271d50582c2e2a3ea61edd5eab3557640
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-17 22:28:02 -0700

    LC-minimum-difference-in-sums-after-removal-of-elements [Runtime: 188 ms; Memory: 83.1 MB]

--- heap-priority-queue/minimum-difference-in-sums-after-removal-of-elements.java
+++ heap-priority-queue/minimum-difference-in-sums-after-removal-of-elements.java
@@ -55,6 +55,6 @@ class Solution {
         Solution sol = new Solution();
 
         System.out.println(sol.minimumDifference(new int[]{3, 1, 2})); // Output: -1
-        System.out.println(sol.minimumDifference(new int[]{7, 9, 5, 8, 1, 3})); // Output: 1
+        System.out.println(sol.minimumDifference(new int[]{7, 9, 5, 8, 1, 3}));
     }
 }
