35a408fd8a7b5f081652f0fe3c8468780131c520
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-17 22:27:51 -0700

    LC-minimum-difference-in-sums-after-removal-of-elements [Runtime: 181 ms; Memory: 83 MB]

---
+++ array/minimum-difference-in-sums-after-removal-of-elements.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    public long minimumDifference(int[] nums) {
+        int n = nums.length / 3;
+        int len = nums.length;
+
+        long[] leftSums = new long[len];
+        long[] rightSums = new long[len];
+
+        // Max heap for left n smallest sum
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        long leftSum = 0;
+        for (int i = 0; i < 2 * n; i++) {
+            maxHeap.offer(nums[i]);
+            leftSum += nums[i];
+            if (maxHeap.size() > n) {
+                leftSum -= maxHeap.poll();
+            }
+            if (maxHeap.size() == n) {
+                leftSums[i] = leftSum;
+            } else {
+                leftSums[i] = Long.MAX_VALUE;
+            }
+        }
+
+        // Min heap for right n largest sum
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long rightSum = 0;
+        for (int i = len - 1; i >= n; i--) {
+            minHeap.offer(nums[i]);
+            rightSum += nums[i];
+            if (minHeap.size() > n) {
+                rightSum -= minHeap.poll();
+            }
+            if (minHeap.size() == n) {
+                rightSums[i] = rightSum;
+            } else {
+                rightSums[i] = Long.MIN_VALUE;
+            }
+        }
+
+        // Calculate the minimum difference
+        long minDiff = Long.MAX_VALUE;
+        for (int i = n - 1; i < 2 * n; i++) {
+            if (leftSums[i] != Long.MAX_VALUE && rightSums[i + 1] != Long.MIN_VALUE) {
+                minDiff = Math.min(minDiff, leftSums[i] - rightSums[i + 1]);
+            }
+        }
+
+        return minDiff;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.minimumDifference(new int[]{3, 1, 2})); // Output: -1
+        System.out.println(sol.minimumDifference(new int[]{7, 9, 5, 8, 1, 3})); // Output: 1
+    }
+}
