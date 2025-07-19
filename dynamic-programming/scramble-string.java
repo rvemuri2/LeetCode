31d2aa9648423ebcea29cb9811f2764f31038806
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-18 18:34:25 -0700

    LC-scramble-string [Runtime: 11 ms; Memory: 45.1 MB]

--- dynamic-programming/scramble-string.java
+++ dynamic-programming/scramble-string.java
@@ -27,14 +27,12 @@ class Solution {
         int n = s1.length();
         // Try all possible splits
         for (int i = 1; i < n; i++) {
-            // Without swap
             if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
                 isScramble(s1.substring(i), s2.substring(i))) {
                 memo.put(key, true);
                 return true;
             }
 
-            // With swap
             if (isScramble(s1.substring(0, i), s2.substring(n - i)) &&
                 isScramble(s1.substring(i), s2.substring(0, n - i))) {
                 memo.put(key, true);
