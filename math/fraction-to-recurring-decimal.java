1bf740a77dbb80570d0f0faa71f7fb223809164b
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-20 14:42:53 -0700

    LC-fraction-to-recurring-decimal [Runtime: 1 ms; Memory: 41.3 MB]

--- math/fraction-to-recurring-decimal.java
+++ math/fraction-to-recurring-decimal.java
@@ -46,6 +46,5 @@ class Solution {
         // ✅ Test cases
         System.out.println(sol.fractionToDecimal(1, 2));      // 0.5
         System.out.println(sol.fractionToDecimal(2, 1));      // 2
-        System.out.println(sol.fractionToDecimal(4, 333));
     }
 }
