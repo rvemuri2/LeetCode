7dea35634ad6397ccfdec0ea420ca2ce969a730b
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-19 21:48:55 -0700

    LC-compare-version-numbers [Runtime: 1 ms; Memory: 41.8 MB]

--- string/compare-version-numbers.java
+++ string/compare-version-numbers.java
@@ -42,6 +42,6 @@ class Solution {
 
         System.out.println("Test 7: " + sol.compareVersion("1.0.2", "1.0.10"));  // -1
 
-        System.out.println("Test 8: " + sol.compareVersion("2.0", "1.9.9"));  // 1
+        System.out.println("Test 8: " + sol.compareVersion("2.0", "1.9.9"));
     }
 }
