79d61b6da1a1bcc6e26a24496770248adf3b75b0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-20 14:38:04 -0700

    LC-compare-version-numbers [Runtime: 1 ms; Memory: 41.8 MB]

--- two-pointers/compare-version-numbers.java
+++ two-pointers/compare-version-numbers.java
@@ -1,6 +1,4 @@
 class Solution {
-    /**
-     */
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
