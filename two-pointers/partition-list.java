60aa702407fd46f801d74985e46e8a2d76c7d361
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-22 13:30:13 -0700

    LC-partition-list [Runtime: 0 ms; Memory: 42.6 MB]

--- two-pointers/partition-list.java
+++ two-pointers/partition-list.java
@@ -46,7 +46,6 @@ public static void main(String[] args) {
     Solution sol = new Solution();
 
     ListNode head1 = new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(5, new ListNode(2))))));
-    printList(sol.partition(head1, 3));
     }
 
 }
