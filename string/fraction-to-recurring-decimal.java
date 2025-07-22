942a68c9bcc91392dd743c63bb2af01015cd95a0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-21 23:16:25 -0700

    LC-fraction-to-recurring-decimal [Runtime: 1 ms; Memory: 40.8 MB]

--- string/fraction-to-recurring-decimal.java
+++ string/fraction-to-recurring-decimal.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0) return "0";
+        if (numerator == 0) {
+            return "0";
+        }
 
         StringBuilder sb = new StringBuilder();
 
