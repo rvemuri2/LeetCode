492e468c1ec94ceb28063bec493131a174ad76af
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-21 23:23:28 -0700

    LC-gray-code [Runtime: 4 ms; Memory: 51.9 MB]

--- backtracking/gray-code.java
+++ backtracking/gray-code.java
@@ -8,7 +8,7 @@ class Solution {
      */
     public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<>();
-        int total = 1 << n; // 2^n values
+        int total = 1 << n;
 
         for (int i = 0; i < total; i++) {
             result.add(i ^ (i >> 1));
