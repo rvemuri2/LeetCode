4717c46eb0794e3c1bce9f8aabb9e3e750ab93d3
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-22 13:30:23 -0700

    LC-partition-list [Runtime: 0 ms; Memory: 42.5 MB]

--- linked-list/partition-list.java
+++ linked-list/partition-list.java
@@ -44,8 +44,6 @@ class Solution {
 
 public static void main(String[] args) {
     Solution sol = new Solution();
-
-    ListNode head1 = new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(5, new ListNode(2))))));
     }
 
 }
