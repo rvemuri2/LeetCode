e2f8cd97d03b0569bdda95e74bf4602398bd6ede
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-23 19:06:18 -0700

    LC-search-in-rotated-sorted-array-ii [Runtime: 0 ms; Memory: 43.1 MB]

--- binary-search/search-in-rotated-sorted-array-ii.java
+++ binary-search/search-in-rotated-sorted-array-ii.java
@@ -37,6 +37,6 @@ public static void main(String[] args) {
     System.out.println(sol.search(new int[]{2,5,6,0,0,1,2}, 3)); // false
     System.out.println(sol.search(new int[]{1,0,1,1,1}, 0));     // true
     System.out.println(sol.search(new int[]{1,1,1,1,1}, 0));     // false
-    System.out.println(sol.search(new int[]{1,1,3,1}, 3));       // true
+    System.out.println(sol.search(new int[]{1,1,3,1}, 3));
 }
 }
