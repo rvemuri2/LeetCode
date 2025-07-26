1acce6689a79d21f11a3e7b8ec6f6143aa17e744
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-25 23:51:29 -0700

    LC-h-index [Runtime: 2 ms; Memory: 41.7 MB]

--- array/h-index.java
+++ array/h-index.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
-
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int n = citations.length;
-
         for (int i = 0; i < n; i++) {
             int h = n - i;
             if (citations[i] >= h)
