d63f02624a599831e2240f0d48d2de4f7cba5f7e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-25 23:49:26 -0700

    LC-h-index [Runtime: 2 ms; Memory: 41.6 MB]

--- array/h-index.java
+++ array/h-index.java
@@ -20,6 +20,5 @@ class Solution {
 
         System.out.println("Test Case 1: " + sol.hIndex(new int[]{3, 0, 6, 1, 5})); // Output: 3
         System.out.println("Test Case 2: " + sol.hIndex(new int[]{1, 3, 1}));       // Output: 1
-        System.out.println("Test Case 3: " + sol.hIndex(new int[]{0, 0, 0}));
     }
 }
