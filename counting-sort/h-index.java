4b3742102b47f3cf79cb987f21672a4b2ace374c
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-25 23:48:53 -0700

    LC-h-index [Runtime: 2 ms; Memory: 41.5 MB]

--- counting-sort/h-index.java
+++ counting-sort/h-index.java
@@ -21,6 +21,6 @@ class Solution {
         System.out.println("Test Case 1: " + sol.hIndex(new int[]{3, 0, 6, 1, 5})); // Output: 3
         System.out.println("Test Case 2: " + sol.hIndex(new int[]{1, 3, 1}));       // Output: 1
         System.out.println("Test Case 3: " + sol.hIndex(new int[]{0, 0, 0}));       // Output: 0
-        System.out.println("Test Case 4: " + sol.hIndex(new int[]{10, 8, 5, 4, 3})); // Output: 4
+        System.out.println("Test Case 4: " + sol.hIndex(new int[]{10, 8, 5, 4, 3}));
     }
 }
