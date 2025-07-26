0eecaa65d179f60cbc9ba836c3e1eed7c6088115
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-25 23:51:08 -0700

    LC-h-index [Runtime: 2 ms; Memory: 41.5 MB]

--- sorting/h-index.java
+++ sorting/h-index.java
@@ -7,9 +7,8 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int h = n - i; // Number of papers with at least citations[i] citations
-            if (citations[i] >= h) {
+            if (citations[i] >= h)
                 return h;
-            }
         }
 
         return 0;
