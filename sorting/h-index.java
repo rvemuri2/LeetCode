3c52e867148054d00364efa00915dcd98144c672
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-25 23:48:41 -0700

    LC-h-index [Runtime: 2 ms; Memory: 41.4 MB]

--- sorting/h-index.java
+++ sorting/h-index.java
@@ -22,6 +22,5 @@ class Solution {
         System.out.println("Test Case 2: " + sol.hIndex(new int[]{1, 3, 1}));       // Output: 1
         System.out.println("Test Case 3: " + sol.hIndex(new int[]{0, 0, 0}));       // Output: 0
         System.out.println("Test Case 4: " + sol.hIndex(new int[]{10, 8, 5, 4, 3})); // Output: 4
-        System.out.println("Test Case 5: " + sol.hIndex(new int[]{100}));
     }
 }
