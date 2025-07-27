13d0b43f353243a0553b8c12d120fd51b0242e22
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-26 21:08:38 -0700

    LC-perfect-squares [Runtime: 137 ms; Memory: 45.2 MB]

--- dynamic-programming/perfect-squares.java
+++ dynamic-programming/perfect-squares.java
@@ -26,6 +26,6 @@ class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println("Test Case 1 (n=12): " + sol.numSquares(12)); // Output: 3 (4+4+4)
-        System.out.println("Test Case 2 (n=13): " + sol.numSquares(13)); // Output: 2 (4+9)
+        System.out.println("Test Case 2 (n=13): " + sol.numSquares(13));
     }
 }
