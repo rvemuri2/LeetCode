85168735b76f6ea1e04189638c4275c6f42f2ced
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-26 21:07:40 -0700

    LC-perfect-squares [Runtime: 138 ms; Memory: 45 MB]

--- math/perfect-squares.java
+++ math/perfect-squares.java
@@ -28,6 +28,6 @@ class Solution {
         System.out.println("Test Case 1 (n=12): " + sol.numSquares(12)); // Output: 3 (4+4+4)
         System.out.println("Test Case 2 (n=13): " + sol.numSquares(13)); // Output: 2 (4+9)
         System.out.println("Test Case 3 (n=1): " + sol.numSquares(1));   // Output: 1 (1)
-        System.out.println("Test Case 4 (n=43): " + sol.numSquares(43)); // Output: 3 (36+4+3)
+        System.out.println("Test Case 4 (n=43): " + sol.numSquares(43));
     }
 }
