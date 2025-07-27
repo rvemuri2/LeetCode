f81c46859689cea513689eccefa08354a26bee69
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-26 21:09:45 -0700

    LC-perfect-squares [Runtime: 139 ms; Memory: 44.8 MB]

--- breadth-first-search/perfect-squares.java
+++ breadth-first-search/perfect-squares.java
@@ -25,6 +25,6 @@ class Solution {
     // Sample test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println("Test Case 1 (n=12): " + sol.numSquares(12)); // Output: 3 (4+4+4)
+        System.out.println("Test Case 1 (n=12): " + sol.numSquares(12));
     }
 }
