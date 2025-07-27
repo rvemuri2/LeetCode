720159afa86509a85b39b495edfa0edc196c5dda
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-26 21:12:10 -0700

    LC-perfect-squares [Runtime: 138 ms; Memory: 45 MB]

--- dynamic-programming/perfect-squares.java
+++ dynamic-programming/perfect-squares.java
@@ -4,7 +4,7 @@ class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0; // Base case
+        dp[0] = 0;
 
         List<Integer> squares = new ArrayList<>();
         for (int i = 1; i * i <= n; i++) {
