bbfb1099c01becedc8a92180a4fa4ca93cd34889
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-27 21:57:42 -0700

    LC-peeking-iterator [Runtime: 4 ms; Memory: 42.4 MB]

--- design/peeking-iterator.java
+++ design/peeking-iterator.java
@@ -68,6 +68,5 @@ class PeekingIterator implements Iterator<Integer> {
         System.out.println("\nTest 3:");
         PeekingIterator it3 = new PeekingIterator(Arrays.asList(4, 5).iterator());
         System.out.println(it3.hasNext()); // true
-        System.out.println(it3.peek());
     }
 }
