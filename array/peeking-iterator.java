f7e8bd276dd1b9260a2659f1635fe1969fa17197
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-28 18:04:57 -0700

    LC-peeking-iterator [Runtime: 4 ms; Memory: 42.6 MB]

--- array/peeking-iterator.java
+++ array/peeking-iterator.java
@@ -1,5 +1,4 @@
 // Java Iterator interface reference:
-// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 class PeekingIterator implements Iterator<Integer> {
     private Iterator<Integer> iterator;
