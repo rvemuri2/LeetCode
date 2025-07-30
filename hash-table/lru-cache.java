ad56be18d8c310bcce551e0e5c1a5d070e887733
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-07-29 17:06:12 -0700

    LC-lru-cache [Runtime: 41 ms; Memory: 111.4 MB]

--- hash-table/lru-cache.java
+++ hash-table/lru-cache.java
@@ -131,7 +131,7 @@ class LRUCache {
         lru.put(3, 3);               // evicts key 2; cache: {1=1, 3=3}
         System.out.println(lru.get(2)); // -1
         lru.put(4, 4);               // evicts key 1; cache: {4=4, 3=3}
-        System.out.println(lru.get(1)); // -1
+        System.out.println(lru.get(1));
     }
     ===================================================================== */
 }
