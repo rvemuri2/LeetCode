d07b1a44f791fd2dc53d00ce79d2661bdff744d7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-01 18:23:51 -0700

    LC-surrounded-regions [Runtime: 3 ms; Memory: 45.9 MB]

--- union-find/surrounded-regions.java
+++ union-find/surrounded-regions.java
@@ -43,7 +43,6 @@ class Solution {
             }
         }
 
-        // BFS to mark all 'O's connected to border as SAFE ('S')
         int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
         while (!q.isEmpty()) {
             int[] cell = q.poll();
