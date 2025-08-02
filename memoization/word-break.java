71f75c24f9fcc724b8f4d87f19bd91b269a165e0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-01 18:29:46 -0700

    LC-word-break [Runtime: 8 ms; Memory: 44.9 MB]

--- memoization/word-break.java
+++ memoization/word-break.java
@@ -36,6 +36,5 @@ class Solution {
         System.out.println(sol.wordBreak("leetcode", Arrays.asList("leet", "code"))); // true
         System.out.println(sol.wordBreak("applepenapple", Arrays.asList("apple", "pen"))); // true
         System.out.println(sol.wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat"))); // false
-        System.out.println(sol.wordBreak("aaaaaaa", Arrays.asList("aaaa", "aaa")));
     }
 }
