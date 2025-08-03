93e61714d0e9caa8c88923dabdf02914e4b6aa8d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-02 23:39:35 -0700

    LC-word-break [Runtime: 8 ms; Memory: 44.5 MB]

--- trie/word-break.java
+++ trie/word-break.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 class Solution {
-    /**
-     */
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordSet = new HashSet<>(wordDict);
         int n = s.length();
