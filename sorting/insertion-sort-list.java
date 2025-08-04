390ffdf6a21c7f9158514ec9abfaba7902f42cb7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-03 19:48:48 -0700

    LC-insertion-sort-list [Runtime: 20 ms; Memory: 44.6 MB]

--- sorting/insertion-sort-list.java
+++ sorting/insertion-sort-list.java
@@ -44,7 +44,5 @@ public class Main {
 
         // Build list: 4 -> 2 -> 1 -> 3
         ListNode head = new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
-
-        ListNode sorted = sol.insertionSortList(head);
     }
 }
