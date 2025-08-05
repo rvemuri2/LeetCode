b988b86586567b794f2c76145549a525345c8b92
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-05 10:20:57 -0700

    LC-game-of-life [Runtime: 0 ms; Memory: 41.8 MB]

--- matrix/game-of-life.java
+++ matrix/game-of-life.java
@@ -38,7 +38,9 @@ public class Solution {
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == -1) board[i][j] = 0;
+                if (board[i][j] == -1) {
+                    board[i][j] = 0;
+                }
                 else if (board[i][j] == 2) board[i][j] = 1;
             }
         }
