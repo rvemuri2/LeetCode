7e54887a680d5f2d3dc589ada7f9636f600f989f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-04 23:45:28 -0700

    LC-game-of-life [Runtime: 0 ms; Memory: 41.8 MB]

---
+++ simulation/game-of-life.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+public class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        int[][] dirs = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            { 0, -1},          { 0, 1},
+            { 1, -1}, { 1, 0}, { 1, 1}
+        };
+
+        // First pass: mark the cells with temporary states
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = 0;
+                for (int[] dir : dirs) {
+                    int r = i + dir[0], c = j + dir[1];
+                    if (r >= 0 && r < m && c >= 0 && c < n) {
+                        if (board[r][c] == 1 || board[r][c] == -1) {
+                            liveNeighbors++;
+                        }
+                    }
+                }
+
+                if (board[i][j] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        board[i][j] = -1; // Live -> Dead
+                    }
+                } else {
+                    if (liveNeighbors == 3) {
+                        board[i][j] = 2; // Dead -> Live
+                    }
+                }
+            }
+        }
+
+        // Second pass: finalize the board
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == -1) board[i][j] = 0;
+                else if (board[i][j] == 2) board[i][j] = 1;
+            }
+        }
+    }
+
+    // Utility method to print a 2D board
+    private static void printBoard(int[][] board) {
+        for (int[] row : board) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    // Main method with test cases
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[][] board1 = {
+            {0, 1, 0},
+            {0, 0, 1},
+            {1, 1, 1},
+            {0, 0, 0}
+        };
+
+        System.out.println("Before:");
+        printBoard(board1);
+
+        sol.gameOfLife(board1);
+
+        System.out.println("\nAfter:");
+        printBoard(board1);
+
+        // Additional Test Case
+        int[][] board2 = {
+            {1, 1},
+            {1, 0}
+        };
+
+        System.out.println("\nBefore (Test Case 2):");
+        printBoard(board2);
+
+        sol.gameOfLife(board2);
+
+        System.out.println("\nAfter (Test Case 2):");
+        printBoard(board2);
+    }
+}
