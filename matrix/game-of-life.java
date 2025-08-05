1d1e5ee233a62eed6a4fcce63d73035b1f2dfd0f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-04 23:52:06 -0700

    LC-game-of-life [Runtime: 0 ms; Memory: 41.7 MB]

--- matrix/game-of-life.java
+++ matrix/game-of-life.java
@@ -30,12 +30,12 @@ public class Solution {
                     }
                 } else {
                     if (liveNeighbors == 3) {
-                        board[i][j] = 2; // Dead -> Live
+                        board[i][j] = 2;
                     }
                 }
             }
         }
-        
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == -1) board[i][j] = 0;
