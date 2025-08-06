704ec20097690ba3b476c523ad43ff45d3876300
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-06 14:51:27 -0700

    LC-bulls-and-cows [Runtime: 4 ms; Memory: 42.5 MB]

--- string/bulls-and-cows.java
+++ string/bulls-and-cows.java
@@ -10,9 +10,8 @@ class Solution {
         for (int i = 0; i < secret.length(); i++) {
             char s = secret.charAt(i);
             char g = guess.charAt(i);
-            if (s == g) {
-                bulls++;
-            } else {
+            if (s == g) bulls++;
+            else {
                 secretCount[s - '0']++;
                 guessCount[g - '0']++;
             }
