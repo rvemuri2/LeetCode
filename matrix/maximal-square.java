506b8eba26178bdb78b4ab479644d42588ca31be
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-14 23:39:15 -0700

    LC-maximal-square [Runtime: 7 ms; Memory: 60.5 MB]

--- matrix/maximal-square.java
+++ matrix/maximal-square.java
@@ -6,7 +6,6 @@ class Solution {
      *
      * Let dp[i+1][j+1] be the side length of the largest all-1s square
      * whose bottom-right corner is at matrix[i][j].
-     * Transition (only if matrix[i][j] == '1'):
      */
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length;
