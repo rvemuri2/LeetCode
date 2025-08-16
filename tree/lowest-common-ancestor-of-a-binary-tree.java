5cc9ff322ae8b17ea82f2c67aac16319d84f3a82
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-15 23:42:13 -0700

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: 7 ms; Memory: 45.4 MB]

--- tree/lowest-common-ancestor-of-a-binary-tree.java
+++ tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -37,8 +37,10 @@ class Solution {
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
         // If both sides found a node, current root is the LCA
-        if (left != null && right != null) return root;
-
+        if (left != null && right != null) {
+             return root;
+        }
+        
         return (left != null) ? left : right;
     }
 }
