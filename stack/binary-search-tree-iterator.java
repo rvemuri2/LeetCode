8471d0e2d08631375c5ceda1cefdef062b0f3764
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-20 23:55:59 -0700

    LC-binary-search-tree-iterator [Runtime: 17 ms; Memory: 48.3 MB]

--- stack/binary-search-tree-iterator.java
+++ stack/binary-search-tree-iterator.java
@@ -70,7 +70,6 @@ class BSTIterator {
         System.out.println(it.next());    // 7
         System.out.println(it.hasNext()); // true
         System.out.println(it.next());    // 9
-        System.out.println(it.hasNext());
     }
 }
 
