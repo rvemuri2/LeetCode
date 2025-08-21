f342f152a06d31b84bd8ec863364001b764f75a0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-20 23:50:52 -0700

    LC-factorial-trailing-zeroes [Runtime: 0 ms; Memory: 41 MB]

--- math/factorial-trailing-zeroes.java
+++ math/factorial-trailing-zeroes.java
@@ -4,7 +4,7 @@ class Solution {
         
         // Count multiples of 5, 25, 125, etc.
         while (n > 0) {
-            n /= 5;        // Reduce n by dividing with 5
+            n /= 5;
             count += n;
         }
         
