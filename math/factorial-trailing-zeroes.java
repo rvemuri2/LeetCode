4d178a332671a043c3ea4d5eee39eee9a92425ba
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-20 23:51:05 -0700

    LC-factorial-trailing-zeroes [Runtime: 0 ms; Memory: 40.5 MB]

--- math/factorial-trailing-zeroes.java
+++ math/factorial-trailing-zeroes.java
@@ -2,7 +2,6 @@ class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
         
-        // Count multiples of 5, 25, 125, etc.
         while (n > 0) {
             n /= 5;
             count += n;
