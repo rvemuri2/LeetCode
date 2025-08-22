e7be5b8f84ce91cedae371035b466e400ef092d0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-21 23:48:08 -0700

    LC-binary-search-tree-iterator [Runtime: 16 ms; Memory: 48.1 MB]

--- design/binary-search-tree-iterator.java
+++ design/binary-search-tree-iterator.java
@@ -62,6 +62,5 @@ class BSTIterator {
         root.left = new TreeNode(3);
         root.right = new TreeNode(15);
         root.right.left = new TreeNode(9);
-        root.right.right = new TreeNode(20);
     }
 }
