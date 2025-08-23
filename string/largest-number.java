326ab470a6c2ddaeb883d2c45e9060379f8a4b85
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-22 23:50:26 -0700

    LC-largest-number [Runtime: 5 ms; Memory: 43.1 MB]

--- string/largest-number.java
+++ string/largest-number.java
@@ -23,6 +23,6 @@ class Solution {
         Solution sol = new Solution();
 
         System.out.println(sol.largestNumber(new int[]{10, 2}));                 // "210"
-        System.out.println(sol.largestNumber(new int[]{3, 30, 34, 5, 9}));       // "9534330"
+        System.out.println(sol.largestNumber(new int[]{3, 30, 34, 5, 9}));
     }
 }
