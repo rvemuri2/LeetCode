d7cf109816ff6b00c7255c2fae4c07f65436467a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-24 23:41:53 -0700

    LC-coin-change [Runtime: 126 ms; Memory: 44.7 MB]

--- dynamic-programming/coin-change.java
+++ dynamic-programming/coin-change.java
@@ -28,7 +28,5 @@ class Solution {
     // Quick tests
     public static void main(String[] args) {
         Solution sol = new Solution();
-
-        System.out.println(sol.coinChange(new int[]{1, 2, 5}, 11));
     }
 }
