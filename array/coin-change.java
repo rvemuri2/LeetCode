74d7f5b3469a30203d6535db3d12822ea55fe2d7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-24 23:41:22 -0700

    LC-coin-change [Runtime: 16 ms; Memory: 44.4 MB]

--- array/coin-change.java
+++ array/coin-change.java
@@ -30,6 +30,6 @@ class Solution {
         Solution sol = new Solution();
 
         System.out.println(sol.coinChange(new int[]{1, 2, 5}, 11)); // 3 (5+5+1)
-        System.out.println(sol.coinChange(new int[]{2}, 3));        // -1
+        System.out.println(sol.coinChange(new int[]{2}, 3));
     }
 }
