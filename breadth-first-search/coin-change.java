77d72ac47e32b13f6b1fb6b6b57b33a8dbfe955a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-25 09:35:41 -0700

    LC-coin-change [Runtime: 16 ms; Memory: 44.2 MB]

--- breadth-first-search/coin-change.java
+++ breadth-first-search/coin-change.java
@@ -12,7 +12,7 @@ class Solution {
         if (amount == 0) return 0;
         // dp[a] = fewest coins to make 'a'; initialize to "infinity"
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1); // sentinel larger than any possible answer
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
 
         for (int a = 1; a <= amount; a++) {
