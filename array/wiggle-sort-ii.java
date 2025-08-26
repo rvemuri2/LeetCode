f9822347d15e9f4a3d0fc01656363ad592117c0c
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-25 22:54:42 -0700

    LC-wiggle-sort-ii [Runtime: 55 ms; Memory: 46.9 MB]

--- array/wiggle-sort-ii.java
+++ array/wiggle-sort-ii.java
@@ -88,8 +88,6 @@ public class Solution {
         int[] nums3 = {1,1,2,1,2,2,1};
         sol.wiggleSort(nums3);
         System.out.println(Arrays.toString(nums3)); // valid wiggle pattern
-
-        // quick checker: nums[0] < nums[1] > nums[2] < nums[3] ...
     }
 
     private static boolean isWiggle(int[] a) {
