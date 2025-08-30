291c470bcb79ebf289c9684bb5d7f985340bf8f9
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-29 23:42:35 -0700

    LC-maximum-binary-tree [Runtime: 2 ms; Memory: 45.1 MB]

--- divide-and-conquer/maximum-binary-tree.java
+++ divide-and-conquer/maximum-binary-tree.java
@@ -1,14 +1,27 @@
-public class Solution {
+class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        Deque<TreeNode> st = new ArrayDeque<>();
-        for (int x : nums) {
-            TreeNode curr = new TreeNode(x);
-            while (!st.isEmpty() && st.peek().val < x) curr.left = st.pop();
-            if (!st.isEmpty()) st.peek().right = curr;
-            st.push(curr);
-        }
-        TreeNode root = null;
-        while (!st.isEmpty()) root = st.pop();
+        return helper(nums, 0, nums.length);
+    }
+
+    private TreeNode helper(int[] nums, int l, int r) {
+        if (l == r)
+            return null;
+
+        int max_index = max(nums, l, r);
+        TreeNode root = new TreeNode(nums[max_index]);
+        root.left = helper(nums, l, max_index);
+        root.right = helper(nums, max_index + 1, r);
         return root;
+    }
+
+    private int max(int[] nums, int l, int r) {
+        int max_index = l;
+
+        for (int i = l; i < r; i++) {
+            if (nums[max_index] < nums[i])
+                max_index = i;
+        }
+
+        return max_index;
     }
 }
