8874cdae918872a4377d574a50a131e52c8ce81a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-29 23:38:34 -0700

    LC-maximum-binary-tree [Runtime: 8 ms; Memory: 45 MB]

--- stack/maximum-binary-tree.java
+++ stack/maximum-binary-tree.java
@@ -15,7 +15,6 @@
  */
 public class Solution {
     /**
-     * Construct Maximum Binary Tree in O(n) using a monotonic decreasing stack.
      */
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         Deque<TreeNode> st = new ArrayDeque<>();
