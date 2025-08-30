0634f2eeec2d9fa582ed7036319e203ed39eae0e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-29 23:40:22 -0700

    LC-maximum-binary-tree [Runtime: 8 ms; Memory: 44.6 MB]

--- divide-and-conquer/maximum-binary-tree.java
+++ divide-and-conquer/maximum-binary-tree.java
@@ -3,7 +3,6 @@
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
- *     TreeNode right;
  */
 public class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
