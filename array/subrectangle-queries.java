adb21ab880445618c6e2abe5311d4f5ff89a71a6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-31 15:56:17 -0700

    LC-subrectangle-queries [Runtime: 20 ms; Memory: 48 MB]

--- array/subrectangle-queries.java
+++ array/subrectangle-queries.java
@@ -48,7 +48,6 @@ class Main {
         System.out.println(sq.getValue(0,2)); // 5
         System.out.println(sq.getValue(3,1)); // 5
         sq.updateSubrectangle(3,0,3,2,10);
-        System.out.println(sq.getValue(3,1)); // 10
 ------------------------------------------------------ */
 
 
