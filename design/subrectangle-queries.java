8f13417cd0509eda83efe5eedeaacffa0dc23178
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-31 16:01:26 -0700

    LC-subrectangle-queries [Runtime: 20 ms; Memory: 48.2 MB]

--- design/subrectangle-queries.java
+++ design/subrectangle-queries.java
@@ -4,7 +4,6 @@ class SubrectangleQueries {
     private final int[][] rect; // mutable copy we will update in-place
 
     /**
-     * Store a deep copy of the initial rectangle so we can mutate safely.
      */
     public SubrectangleQueries(int[][] rectangle) {
         int r = rectangle.length, c = rectangle[0].length;
@@ -13,7 +12,7 @@ class SubrectangleQueries {
             System.arraycopy(rectangle[i], 0, rect[i], 0, c);
         }
     }
-    
+
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
         for (int r = row1; r <= row2; r++) {
             for (int c = col1; c <= col2; c++) {
