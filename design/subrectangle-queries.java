1c73408e4eb52f977c823a5ff3a603cb8b60c72f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-08-31 16:01:18 -0700

    LC-subrectangle-queries [Runtime: 20 ms; Memory: 48.1 MB]

--- design/subrectangle-queries.java
+++ design/subrectangle-queries.java
@@ -13,9 +13,7 @@ class SubrectangleQueries {
             System.arraycopy(rectangle[i], 0, rect[i], 0, c);
         }
     }
-
-    /**
-     */
+    
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
         for (int r = row1; r <= row2; r++) {
             for (int c = col1; c <= col2; c++) {
