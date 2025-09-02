b94d9d5ae9761f07f65acf5a69442000c10ef5fd
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-01 23:49:33 -0700

    LC-sort-the-students-by-their-kth-score [Runtime: 2 ms; Memory: 52.5 MB]

--- sorting/sort-the-students-by-their-kth-score.java
+++ sorting/sort-the-students-by-their-kth-score.java
@@ -5,7 +5,6 @@ import java.util.*;
  */
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-        // Sort in-place by the kth exam descending.
         final int kk = k;
         Arrays.sort(score, (a, b) -> Integer.compare(b[kk], a[kk]));
         return score;
