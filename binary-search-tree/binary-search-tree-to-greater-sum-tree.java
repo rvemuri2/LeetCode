e4e7e3b951cb7f5db172063956058c7600c0babb
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-05 22:52:08 -0700

    LC-binary-search-tree-to-greater-sum-tree [Runtime: 0 ms; Memory: 41.5 MB]

--- binary-search-tree/binary-search-tree-to-greater-sum-tree.java
+++ binary-search-tree/binary-search-tree-to-greater-sum-tree.java
@@ -47,7 +47,9 @@ class Solution {
 
 class Main {
     static TreeNode fromLevelOrder(Integer[] arr) {
-        if (arr == null || arr.length == 0 || arr[0] == null) return null;
+        if (arr == null || arr.length == 0 || arr[0] == null) {
+            return null;
+        }
         TreeNode root = new TreeNode(arr[0]);
         return root;
     }
