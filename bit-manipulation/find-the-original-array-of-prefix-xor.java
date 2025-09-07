861323bd47be85b784da699c23623f70d9a7b36a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-06 23:47:09 -0700

    LC-find-the-original-array-of-prefix-xor [Runtime: 1 ms; Memory: 56.6 MB]

--- bit-manipulation/find-the-original-array-of-prefix-xor.java
+++ bit-manipulation/find-the-original-array-of-prefix-xor.java
@@ -2,7 +2,6 @@ class Solution {
     // Reconstruct arr from its prefix-xor array pref.
     // Key idea:
     //   pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i]
-    // Then for i >= 1:
     public int[] findArray(int[] pref) {
         int n = pref.length;
         int[] arr = new int[n];
