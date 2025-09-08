67831f0234b54823f6ea25b1fa0e16b024abfd1f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-07 19:37:33 -0700

    LC-find-the-prefix-common-array-of-two-arrays [Runtime: 2 ms; Memory: 45.4 MB]

--- hash-table/find-the-prefix-common-array-of-two-arrays.java
+++ hash-table/find-the-prefix-common-array-of-two-arrays.java
@@ -90,6 +90,5 @@ class Solution {
         // Random permutation pair
         int[] A6 = {3, 5, 1, 2, 4};
         int[] B6 = {2, 3, 4, 5, 1};
-        System.out.println("Random: " + Arrays.toString(s.findThePrefixCommonArray(A6, B6)));
     }
 }
