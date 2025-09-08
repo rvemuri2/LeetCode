c1e153242a7d66f3a9f2133d47692082ffe9f968
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-07 23:37:29 -0700

    LC-find-the-prefix-common-array-of-two-arrays [Runtime: 1 ms; Memory: 45.6 MB]

--- hash-table/find-the-prefix-common-array-of-two-arrays.java
+++ hash-table/find-the-prefix-common-array-of-two-arrays.java
@@ -73,6 +73,5 @@ class Solution {
         int[] A4 = {4, 1, 3, 2, 5};
         int[] B4 = {4, 1, 3, 2, 5};
         System.out.println("Same arrays: " + Arrays.toString(s.findThePrefixCommonArray(A4, B4)));
-        // Expected: [1, 2, 3, 4, 5]
     }
 }
