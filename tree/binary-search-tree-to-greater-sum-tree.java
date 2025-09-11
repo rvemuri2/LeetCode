df342128499b8efaaaf63eded74c273f56baa55e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-10 23:45:16 -0700

    LC-binary-search-tree-to-greater-sum-tree [Runtime: 0 ms; Memory: 41.2 MB]

--- tree/binary-search-tree-to-greater-sum-tree.java
+++ tree/binary-search-tree-to-greater-sum-tree.java
@@ -1,18 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int runningSum = 0;
+    private int acc = 0;  // running sum of greater-or-equal values seen so far
+
     public TreeNode bstToGst(TreeNode root) {
-        reverseInOrder(root);
+        reverseInorder(root);
         return root;
     }
 
-    private void reverseInOrder(TreeNode node) {
+    private void reverseInorder(TreeNode node) {
         if (node == null) return;
-
-        reverseInOrder(node.right);
-
-        runningSum += node.val;
-        node.val = runningSum;
-
-        reverseInOrder(node.left);
+        reverseInorder(node.right);   // visit larger values first
+        acc += node.val;              // accumulate
+        node.val = acc;               // update current node
+        reverseInorder(node.left);    // then smaller values
     }
 }
