8052802e9fa848a1b8b667b33db795aea140e302
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-15 20:54:08 -0700

    LC-deepest-leaves-sum [Runtime: 1 ms; Memory: 47.3 MB]

--- binary-tree/deepest-leaves-sum.java
+++ binary-tree/deepest-leaves-sum.java
@@ -65,6 +65,5 @@ class Solution {
         root2.right.left = new TreeNode(1);
         root2.right.right = new TreeNode(3);
         root2.left.left.left = new TreeNode(9);
-        root2.left.right.left = new TreeNode(1);
     }
 }
