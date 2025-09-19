fc5b989bcef134f1370b6ad5c85f3f134c431b37
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-18 23:36:57 -0700

    LC-max-increase-to-keep-city-skyline [Runtime: 0 ms; Memory: 44.7 MB]

--- matrix/max-increase-to-keep-city-skyline.java
+++ matrix/max-increase-to-keep-city-skyline.java
@@ -22,7 +22,6 @@ public class Solution {
             colMax[j] = cm;
         }
 
-        // Sum allowed increases
         int total = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
