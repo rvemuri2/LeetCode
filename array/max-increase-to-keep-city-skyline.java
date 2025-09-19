ba83014ce8fe9d178f87db7c1f6676e9729f6acf
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-18 23:36:39 -0700

    LC-max-increase-to-keep-city-skyline [Runtime: 1 ms; Memory: 44.1 MB]

--- array/max-increase-to-keep-city-skyline.java
+++ array/max-increase-to-keep-city-skyline.java
@@ -27,9 +27,7 @@ public class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int allowed = Math.min(rowMax[i], colMax[j]);
-                if (allowed > grid[i][j]) {
-                    total += allowed - grid[i][j];
-                }
+                if (allowed > grid[i][j]) total += allowed - grid[i][j];
             }
         }
         return total;
