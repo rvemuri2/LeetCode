c314d8b9313d9734995025bbe8f898451304ea16
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-19 23:42:58 -0700

    LC-number-of-laser-beams-in-a-bank [Runtime: 12 ms; Memory: 45.5 MB]

--- array/number-of-laser-beams-in-a-bank.java
+++ array/number-of-laser-beams-in-a-bank.java
@@ -36,6 +36,5 @@ public class Solution {
         // Example 1
         String[] bank1 = {"011001","000000","010100","001000"};
         System.out.println("Test 1: " + sol.numberOfBeams(bank1)); 
-        // Expected: 8
     }
 }
