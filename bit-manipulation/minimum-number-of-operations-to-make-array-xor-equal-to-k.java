15ef1dbfa9e17c66044d744d82e51d34ef893f1d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-22 23:44:14 -0700

    LC-minimum-number-of-operations-to-make-array-xor-equal-to-k [Runtime: 1 ms; Memory: 61.5 MB]

--- bit-manipulation/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ bit-manipulation/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-    // Minimum operations to make XOR of array equal to k
     public int minOperations(int[] nums, int k) {
         int x = 0;
         for (int v : nums)
