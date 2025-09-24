fc1d2e13ca4067449a21c33ccac6898c23360fd4
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-23 23:49:06 -0700

    LC-all-paths-from-source-to-target [Runtime: 2 ms; Memory: 46.4 MB]

---
+++ breadth-first-search/all-paths-from-source-to-target.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(0, graph, path, ans, graph.length - 1);
+        return ans;
+    }
+
+    private void dfs(int node, int[][] graph, List<Integer> path, List<List<Integer>> ans, int target) {
+        if (node == target) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int nxt : graph[node]) {
+            path.add(nxt);
+            dfs(nxt, graph, path, ans, target);
+            path.remove(path.size() - 1); // backtrack
+        }
+    }
+
+    // --- simple test runner ---
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[][] g1 = {{1,2},{3},{3},{}};
+        System.out.println(s.allPathsSourceTarget(g1)); // [[0, 1, 3], [0, 2, 3]]
+
+        int[][] g2 = {{4,3,1},{3,2,4},{3},{4},{}};
+        System.out.println(s.allPathsSourceTarget(g2)); // [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]]
+    }
+}
