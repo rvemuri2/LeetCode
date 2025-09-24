3cea7c9ab830f7700dcc4786f3acc776f3648382
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-23 23:49:20 -0700

    LC-all-paths-from-source-to-target [Runtime: 2 ms; Memory: 46.7 MB]

--- graph/all-paths-from-source-to-target.java
+++ graph/all-paths-from-source-to-target.java
@@ -29,6 +29,6 @@ class Solution {
         System.out.println(s.allPathsSourceTarget(g1)); // [[0, 1, 3], [0, 2, 3]]
 
         int[][] g2 = {{4,3,1},{3,2,4},{3},{4},{}};
-        System.out.println(s.allPathsSourceTarget(g2)); // [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]]
+        System.out.println(s.allPathsSourceTarget(g2));
     }
 }
