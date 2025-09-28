13b551516092a8e7a2451454399e6f4fedbe05cf
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-27 22:52:57 -0700

    LC-shortest-palindrome [Runtime: 9 ms; Memory: 46.4 MB]

---
+++ string-matching/shortest-palindrome.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() <= 1) return s;
+        String rs = new StringBuilder(s).reverse().toString();
+        String t = s + "#" + rs;
+        int[] lps = buildLPS(t);
+        int longestPrefixPal = lps[t.length() - 1]; // length of longest palindromic prefix
+        String suffix = s.substring(longestPrefixPal);
+        return new StringBuilder(suffix).reverse().toString() + s;
+    }
+
+    // KMP prefix-function (LPS array)
+    private int[] buildLPS(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0;
+        for (int i = 1; i < n; i++) {
+            while (len > 0 && str.charAt(i) != str.charAt(len)) {
+                len = lps[len - 1];
+            }
+            if (str.charAt(i) == str.charAt(len)) len++;
+            lps[i] = len;
+        }
+        return lps;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String[] tests = {
+            "aacecaaa",   // -> "aaacecaaa"
+            "abcd",       // -> "dcbabcd"
+            "",           // -> ""
+            "a",          // -> "a"
+            "aa",         // -> "aa"
+            "aba",        // -> "aba"
+            "abb",        // -> "bbabb"
+            "baa",        // -> "aabaa"
+            "aaaaab",     // -> "baaaaab"
+            "abcdabc"     // -> "cbadcabcdabc"
+        };
+
+        for (String s : tests) {
+            System.out.println("Input: \"" + s + "\"  ->  Output: \"" + sol.shortestPalindrome(s) + "\"");
+        }
+    }
+}
