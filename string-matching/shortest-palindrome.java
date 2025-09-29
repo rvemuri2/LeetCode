b39b49dff3a748e9158e098bdd1d7663a8c11107
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-28 23:56:18 -0700

    LC-shortest-palindrome [Runtime: 8 ms; Memory: 45 MB]

--- string-matching/shortest-palindrome.java
+++ string-matching/shortest-palindrome.java
@@ -4,8 +4,7 @@ class Solution {
     for (int i = s.length() - 1; i >= 0; i--) {
         if (s.charAt(i) == s.charAt(j)) j += 1; 
     }
-    if (j == s.length())
-        return s; 
+    if (j == s.length()) return s; 
     String suffix = s.substring(j);
     return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
     }
