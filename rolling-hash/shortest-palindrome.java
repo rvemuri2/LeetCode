55899d505bf17afe612bcd3e1c4323f98c66887f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-28 23:55:00 -0700

    LC-shortest-palindrome [Runtime: 7 ms; Memory: 44.8 MB]

--- rolling-hash/shortest-palindrome.java
+++ rolling-hash/shortest-palindrome.java
@@ -3,7 +3,8 @@ class Solution {
     int j = 0;
     for (int i = s.length() - 1; i >= 0; i--) {
         if (s.charAt(i) == s.charAt(j)) { 
-            j += 1; }
+            j += 1; 
+        }
     }
     if (j == s.length()) { return s; }
     String suffix = s.substring(j);
