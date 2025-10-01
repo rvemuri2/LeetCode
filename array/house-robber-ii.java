289126ea2ca9cff20f07bcf3aae685b3b26dad9b
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-30 22:32:43 -0700

    LC-house-robber-ii [Runtime: 0 ms; Memory: 41.5 MB]

--- array/house-robber-ii.java
+++ array/house-robber-ii.java
@@ -3,9 +3,8 @@ import java.util.*;
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) {
+        if (n == 1)
             return nums[0];
-        }
         int a = robLinear(nums, 0, n - 2);
         int b = robLinear(nums, 1, n - 1);
         return Math.max(a, b);
