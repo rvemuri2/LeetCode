6aa4d1a105db0f03629f3ed65979270c1b9c4392
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-09-30 22:27:36 -0700

    LC-house-robber-ii [Runtime: 0 ms; Memory: 40.9 MB]

--- array/house-robber-ii.java
+++ array/house-robber-ii.java
@@ -28,7 +28,6 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] tests = {
-        };
+        int[][] tests = {};
     }
 }
