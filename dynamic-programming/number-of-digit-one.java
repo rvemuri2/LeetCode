33310966f9df090db31d17722f311e6884a9709d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-12 23:55:01 -0700

    LC-number-of-digit-one [Runtime: 0 ms; Memory: 41 MB]

---
+++ dynamic-programming/number-of-digit-one.java
@@ -0,0 +1,39 @@
+/*If curr == 0 → count = high × position
+If curr == 1 → count = (high × position) + (low + 1)
+If curr > 1 → count = (high + 1) × position */
+
+class Solution {
+    public int countDigitOne(int n) {
+        int b = n;
+        int count = 0;
+        int total = 0;
+
+        while (b != 0) {
+            int position = (int)Math.pow(10, count);  // ✅ position (1,10,100..)
+            int curr = (n / position) % 10;           // ✅ current digit at pos
+
+            count++;
+
+            int a = count;
+            int mul = 1;
+            while (a != 0) {   // ✅ keep your loop
+                mul = mul * 10;
+                a--;
+            }
+
+            int left = n / mul;        
+            int right = n % position;  
+
+            if (curr == 0) {
+                total += left * position;
+            } else if (curr == 1) {
+                total += (left * position) + (right + 1);
+            } else if (curr > 1) {
+                total += (left + 1) * position;
+            }
+
+            b = b / 10;   
+        }
+        return total;
+    }
+}
