0876d30f1837ec4ace1698fec047d3d8fd5dc17f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-13 22:45:10 -0700

    LC-palindrome-linked-list [Runtime: 7 ms; Memory: 56.3 MB]

--- recursion/palindrome-linked-list.java
+++ recursion/palindrome-linked-list.java
@@ -5,7 +5,7 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
