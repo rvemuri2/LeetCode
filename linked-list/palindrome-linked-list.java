5c69a7bb8eb05407d85cbf0328e91188bbd87397
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-13 22:53:41 -0700

    LC-palindrome-linked-list [Runtime: 7 ms; Memory: 56.5 MB]

--- linked-list/palindrome-linked-list.java
+++ linked-list/palindrome-linked-list.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> list = new ArrayList();
-        
+
         while(head != null) {
             list.add(head.val);
             head = head.next;
@@ -9,6 +9,7 @@ class Solution {
         
         int left = 0;
         int right = list.size()-1;
+        
         while(left < right && list.get(left) == list.get(right)) {
             left++;
             right--;
