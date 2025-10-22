e91b2aab5b711d750d41ad1b357d9b113d4dc59c
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-21 23:41:24 -0700

    LC-patching-array [Runtime: 0 ms; Memory: 43.7 MB]

--- array/patching-array.java
+++ array/patching-array.java
@@ -3,7 +3,6 @@ class Solution {
         long miss = 1;
         int result = 0;
         int i = 0;
-
         while (miss <= n) {
             if (i < nums.length && nums[i] <= miss) {
                 miss += nums[i];
