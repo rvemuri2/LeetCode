696f84858bd38818015a39c7a7dead943db4c60d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-22 22:58:00 -0700

    LC-count-of-range-sum [Runtime: 387 ms; Memory: 83.1 MB]

--- ordered-set/count-of-range-sum.java
+++ ordered-set/count-of-range-sum.java
@@ -103,8 +103,8 @@ class Solution {
         
         // for storing first index of number
         for(int i=0;i<n;i++)
-        
-            if(map.get(sum[i])==null){
+
+            if(map.get(sum[i]) == null){
                 map.put(sum[i],i);
                 mapCount.put(sum[i],0);
             }
