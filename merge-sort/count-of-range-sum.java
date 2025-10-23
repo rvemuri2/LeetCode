23d119afe58d75e6b992bcf719839f3c16cbf508
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-22 22:43:33 -0700

    LC-count-of-range-sum [Runtime: 428 ms; Memory: 83.3 MB]

---
+++ merge-sort/count-of-range-sum.java
@@ -0,0 +1,140 @@
+// created bst class for floor and ceil operations cleanly
+// Using OOPS concept to have readable code
+
+class BST{
+    long arr[];
+    int n;
+    
+    BST(long arr[]){
+        this.arr=arr;
+        this.n=arr.length;
+    }
+    
+    int ceil(long num){
+        int lo=0,hi=n-1,res=-1;
+        while(hi>=lo){
+            int mid=(lo+hi)/2;
+            if(arr[mid]>=num){
+                res=mid;
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+        return res;
+    }
+    
+    int floor(long num){
+        int lo=0,hi=n-1,res=-1;
+        while(hi>=lo){
+            int mid=(lo+hi)/2;
+            if(arr[mid]<=num){
+                res=mid;
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    
+    // segment tree array
+    int seg[];
+    
+    // Having prefix-sum array in sorted order
+    long sum[];
+    
+    // storing first index of number in sum
+    Map<Long,Integer>map;
+    
+    // storing number of times number has gone in current loop
+    Map<Long,Integer>mapCount;
+    
+    // Build the segment tree
+    private int build(int left,int right,int index){
+        if(left==right)
+            return seg[index]=1;
+        int mid=(left+right)/2;
+        return seg[index]=build(left,mid,2*index+1)+build(mid+1,right,2*index+2);
+    }
+    
+    // update segment tree by index
+    private int update(int left,int right,int in,int index){
+        if(left>in||right<in)
+            return seg[index];
+        if(left==right)
+            return seg[index]=0;
+        int mid=(left+right)/2;
+        return seg[index]=update(left,mid,in,2*index+1)+update(mid+1,right,in,2*index+2);
+    }
+    
+    // count number of numbers present between l and r
+    private int count(int left,int right,int l,int r,int index){
+        if(right<l||left>r)
+            return 0;
+        if(left>=l&&right<=r)
+            return seg[index];
+        int mid=(left+right)/2;
+        return count(left,mid,l,r,2*index+1)+count(mid+1,right,l,r,2*index+2);
+    }
+    
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        int n=nums.length;
+        
+        // declaring all global variables
+        seg=new int[4*n];
+        sum=new long[n];
+        map=new HashMap<>();
+        mapCount=new HashMap<>();
+        
+        // prefix-sum
+        sum[0]=nums[0];
+        for(int i=1;i<n;i++)
+            sum[i]=sum[i-1]+nums[i];
+        
+        // sort the prefix-sum array
+        Arrays.sort(sum);
+        
+        // build seg tree
+        build(0,n-1,0);
+        
+        // for storing first index of number
+        for(int i=0;i<n;i++)
+            if(map.get(sum[i])==null){
+                map.put(sum[i],i);
+                mapCount.put(sum[i],0);
+            }
+        
+        // for count of sub-arrays and this is our return result variable
+        int res=0;
+        //for current sum
+        long mid=0;
+        
+        // declaring bst class
+        BST bst=new BST(sum);
+        
+        // main logic of our whole code
+        for(int i=0;i<n;i++){
+            
+            // floor for mid+upper because we want less or equal operation
+            int fl=bst.floor(mid+upper);
+            
+            // ceil for mid+lower because we want greater or equal operation
+            int ce=bst.ceil(mid+lower);
+            
+            if(fl!=-1&&ce!=-1&&fl>=ce)
+                res+=count(0,n-1,ce,fl,0);
+            
+            // having current sum
+            mid+=nums[i];
+            
+            // map.get(mid)+mapCount.get(mid) gives the current index of mid in sum
+            update(0,n-1,map.get(mid)+mapCount.get(mid),0);
+            mapCount.put(mid,mapCount.get(mid)+1);
+        }
+        return res;
+    }
+}
