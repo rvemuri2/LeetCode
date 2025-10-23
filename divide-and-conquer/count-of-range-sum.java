812a710e914f65db34a40156a3015f5e3d81d523
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-23 14:53:17 -0700

    LC-count-of-range-sum [Runtime: 422 ms; Memory: 82.8 MB]

--- divide-and-conquer/count-of-range-sum.java
+++ divide-and-conquer/count-of-range-sum.java
@@ -122,7 +122,7 @@ class Solution {
             
             mid += nums[i];
             
-            update(0,n-1,map.get(mid)+mapCount.get(mid),0);
+            update(0, n-1, map.get(mid) + mapCount.get(mid), 0);
 
             mapCount.put(mid,mapCount.get(mid)+1);
         }
