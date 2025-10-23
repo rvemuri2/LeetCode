017c3e18b559e5ce146b52c6e221ffb01e86b6a7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-22 22:58:53 -0700

    LC-count-of-range-sum [Runtime: 407 ms; Memory: 83 MB]

--- segment-tree/count-of-range-sum.java
+++ segment-tree/count-of-range-sum.java
@@ -101,7 +101,7 @@ class Solution {
         // build seg tree
         build(0,n-1,0);
         
-        // for storing first index of number
+        
         for(int i = 0; i < n; i++)
 
             if(map.get(sum[i]) == null){
