4b81b7a27a552a763c572783e3aa733927581169
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-12-09 00:24:00 -0800

    LC-minimum-size-subarray-sum [Runtime: 16 ms; Memory: 28.3 MB]

--- binary-search/minimum-size-subarray-sum.py
+++ binary-search/minimum-size-subarray-sum.py
@@ -5,13 +5,20 @@ class Solution:
         min_length = float('inf')
         sum1 = 0
 
-        for i in nums:
-            sum1 += i
-            count += 1
-            if(sum1 >= target):
-                min_length = min(count, min_length)
-                sum1 = i
-                count = 1
+        l = 0
+        r = 0
 
+        while(l < len(nums) and r < len(nums)):
+
+            sum1 += nums[r]
+            count += 1
+            r += 1
+            
+            while(sum1 >= target):
+                min_length = min(count, min_length)
+                sum1 -= nums[l]
+                count -= 1
+                l += 1
+            
         return 0 if min_length == float('inf') else min_length
         
